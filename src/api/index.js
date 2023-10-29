bf736910aadc7c83a362092fbba31f67d4e7f944
Isanin Sergey <sergey.isanin@gmail.com>
2023-10-29 16:32:43 +0300

    +

--- src/api/index.js
+++ src/api/index.js
@@ -11,3 +11,23 @@ export async function fetchTools() {
     console.error('There has been a problem with your fetch operation:', error);
   }
 }
+
+export async function addTool(toolName) {
+  try {
+    const response = await fetch('http://localhost:4000/add-tool', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ name: toolName }),
+    });
+    if (!response.ok) {
+      throw new Error('Network response was not ok ' + response.statusText);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('There has been a problem with your fetch operation:', error);
+  }
+}
+
--- src/views/Tool.vue
+++ src/views/Tool.vue
@@ -1,29 +1,48 @@
 <template>
-  <v-table>
-    <thead>
-    <tr>
-      <th class='text-left'> Name</th>
-    </tr>
-    </thead>
-    <tbody>
-    <tr v-for='tool in tools' :key='tool.id'>
-      <td>{{ tool.name }}</td>
-    </tr>
-    </tbody>
-  </v-table>
+  <div>
+    <v-text-field v-model="toolName" label="Tool name"></v-text-field>
+    <v-btn @click="addTool">Add Tool</v-btn>
+    <v-table v-if="tools.length > 0">
+      <thead>
+      <tr>
+        <th class='text-left'> Name</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for='tool in tools' :key='tool.id'>
+        <td>{{ tool.name }}</td>
+      </tr>
+      </tbody>
+    </v-table>
+    <div v-else>No tools available</div>
+  </div>
 </template>
 
 <script>
-import { fetchTools } from '@/api'
+import { fetchTools, addTool as apiAddTool } from '@/api'
 
 export default {
   data() {
     return {
       tools: [],
+      toolName: '',
     }
   },
   async created() {
     this.tools = await fetchTools()
+  },
+  methods: {
+    async addTool() {
+      if (this.toolName) {
+        const newTool = await apiAddTool(this.toolName);
+        if (newTool) {
+          this.tools.push(newTool);
+          this.toolName = '';
+        } else {
+          console.error('Failed to add tool');
+        }
+      }
+    }
   },
 }
 </script>
