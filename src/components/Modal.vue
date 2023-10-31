f8525195127c355c3aa823a645e3869a60889e9f
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2023-10-31 13:24:23 +0300

    Update Modal.vue

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,9 +1,41 @@
 <template>
-  <v-dialog :value="value" @input="$emit('input', $event)" width="1024">
+  <v-dialog :value='value' @input="$emit('input', $event)" width='1024'>
     <v-card>
       <v-card-title>
-        Simple Dialog
+        Добавить инструмент
       </v-card-title>
+      <v-card-text>
+        <v-container>
+          <v-row>
+            <v-col sm='6' md='4'>
+              <v-text-field label='Название (Тип)' v-model='typeName' required></v-text-field>
+              <v-text-field label='Группа' v-model='groupName' required></v-text-field>
+              <v-text-field label='Применяемость материала' v-model='materialUsage' required></v-text-field>
+              <v-text-field label='Маркировка' v-model='marking' required></v-text-field>
+              <v-text-field label='Количесво на складе' v-model='stockQuantity' required></v-text-field>
+              <v-text-field label='Нормальный запас на неделю' v-model='weeklySupply' required></v-text-field>
+              <v-text-field label='Заказ' v-model='order' required></v-text-field>
+              <v-select
+                :items="radiusOptions"
+                label="Радиус"
+                v-model="radius"
+                required
+              ></v-select>
+              <!-- Остальные поля -->
+            </v-col>
+            <!-- Добавьте дополнительные поля по аналогии -->
+          </v-row>
+        </v-container>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color='blue-darken-1' text @click="closeDialog">
+          Закрыть
+        </v-btn>
+        <v-btn prepend-icon="mdi-check-circle" color='elevated' text @click='save'>
+          Сохранить
+        </v-btn>
+      </v-card-actions>
     </v-card>
   </v-dialog>
 </template>
@@ -14,6 +46,37 @@ export default {
     value: {
       type: Boolean,
       default: false,
+    },
+  },
+  data() {
+    return {
+      typeName: '',
+      groupName: '',
+      materialUsage: '',
+      marking: '',
+      stockQuantity: '',
+      weeklySupply: '',
+      order: '',
+      radiusOptions: [0.2, 0.4, 0.6, 0.8, 1.0, 1.2],
+      radius: 0.2,  // значение по умолчанию
+    }
+  },
+  methods: {
+    closeDialog() {
+      this.$emit('input', false);
+    },
+    save() {
+      console.log('save method called in Modal component')
+      this.$emit('save', {
+        typeName: this.typeName,
+        groupName: this.groupName,
+        materialUsage: this.materialUsage,
+        marking: this.marking,
+        stockQuantity: this.stockQuantity,
+        weeklySupply: this.weeklySupply,
+        order: this.order,
+        radius: this.radius,
+      })
     },
   },
 }
