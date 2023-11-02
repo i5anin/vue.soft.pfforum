502f44abad83013208f872cf746bfab68a21c833
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2023-11-02 11:38:18 +0300

    Update EditToolModal.vue

--- src/modules/tool/components/EditToolModal.vue
+++ src/modules/tool/components/EditToolModal.vue
@@ -1,15 +1,19 @@
 <template>
-  <Modal :title='popupTitle'>
-    <template #content>
-      <v-container>
-        <v-row>
-          <v-col>
-            <v-combobox label='Название (Тип)' v-model='toolModel.type_name' :items='typeOptions' required></v-combobox>
-            <v-combobox label='Группа' v-model='toolModel.group_name' :items='groupOptions' required></v-combobox>
-            <v-combobox label='Применяемость материала' v-model='toolModel.mat_name' :items='materialOptions' required></v-combobox>
+  <!-- Определение шаблона компонента -->
+  <Modal :title='popupTitle'>  <!-- Привязка заголовка модального окна к переменной popupTitle -->
+    <template #content>  <!-- Слот для содержимого модального окна -->
+      <v-container>  <!-- Контейнер для разметки содержимого -->
+        <v-row>  <!-- Ряд для группировки элементов -->
+          <v-col>  <!-- Колонка для размещения элементов -->
+            <!-- Комбобоксы и текстовые поля для ввода данных -->
+            <!-- Каждый элемент привязан к соответствующему свойству объекта toolModel и имеет свой лейбл -->
+            <v-combobox label='Название (Тип)' v-model='toolModel.type_name' :items='typeOptions' required />
+            <v-combobox label='Группа' v-model='toolModel.group_name' :items='groupOptions' required />
+            <v-combobox label='Применяемость материала' v-model='toolModel.mat_name' :items='materialOptions'
+                        required></v-combobox>
 
             <v-text-field label='Маркировка' v-model='toolModel.name' required />
-            <v-text-field label='Количесво на складе' v-model='toolModel.kolvo_sklad' required />
+            <v-text-field label='Количество на складе' v-model='toolModel.kolvo_sklad' required />
             <v-text-field label='Нормальный запас на неделю' v-model='toolModel.norma' required />
             <v-text-field label='Заказ' v-model='toolModel.zakaz' required />
 
@@ -18,18 +22,13 @@
         </v-row>
       </v-container>
     </template>
-    <template #action>
+    <template #action>  <!-- Слот для действий модального окна -->
+      <!-- Кнопки для закрытия модального окна и сохранения данных -->
       <v-btn color='red darken-1' variant='text' @click='onCancel' class='text-none text-subtitle-1 ml-3'>
         Закрыть
       </v-btn>
-      <v-btn
-        prepend-icon='mdi-check-circle'
-        @click='onSave'
-        class='text-none text-subtitle-1 pl-3'
-        color='blue darken-1'
-        size='large'
-        variant='flat'
-      >
+      <v-btn prepend-icon='mdi-check-circle' @click='onSave' class='text-none text-subtitle-1 pl-3'
+             color='blue darken-1' size='large' variant='flat'>
         Сохранить
       </v-btn>
     </template>
@@ -38,15 +37,17 @@
 
 
 <script>
+// Импорт других компонентов и функций
 import Modal from '@/components/shared/Modal.vue'
-import { fetchTools } from '@/api/api'  // Убедитесь, что импортировали функцию fetchTools
+import { fetchTools } from '@/api/api'
 
+// Экспорт компонента
 export default {
-  name: 'EditToolModal',
-  props: {
+  name: 'EditToolModal',  // Имя компонента
+  props: {  // Входные свойства компонента
     tool: {
-      type: Object,
-      default: () => ({
+      type: Object,  // Тип свойства
+      default: () => ({  // Значение по умолчанию
         id: null,
         group_name: '',
         type_name: '',
@@ -59,48 +60,47 @@ export default {
       }),
     },
     radiusOptions: {
-      type: Array,
+      type: Array,  // Тип свойства
     },
   },
-  components: {
-    Modal,
-  },
-  data: () => ({
-    toolModel: null,
-    typeOptions: [],
-    groupOptions: [],
-    materialOptions: [],
+  components: { Modal }, // Список компонентов, используемых в данном компоненте
+  data: () => ({  // Локальное состояние компонента
+    toolModel: null,  // Модель данных инструмента
+    typeOptions: [],  // Опции для выбора типа
+    groupOptions: [],  // Опции для выбора группы
+    materialOptions: [],  // Опции для выбора материала
   }),
-  watch: {
+  watch: {  // Слежка за изменением свойств и данных
     tool: {
-      immediate: true,
-      handler(tool) {
-        this.toolModel = JSON.parse(JSON.stringify(tool))
+      immediate: true,  // Немедленное выполнение при инициализации
+      handler(tool) {  // Обработчик изменения свойства
+        this.toolModel = JSON.parse(JSON.stringify(tool))  // Клонирование объекта инструмента
       },
     },
   },
-  async created() {
-    const rawData = await fetchTools();
-
-    this.typeOptions = rawData.types.map(type => type.type_name);
-    this.groupOptions = rawData.groups.map(group => group.group_name);
-    this.materialOptions = rawData.materials.map(material => material.mat_name);
+  async created() {  // Хук жизненного цикла, вызывается при создании компонента
+    const rawData = await fetchTools()  // Получение данных с сервера
+    console.log(rawData)
+    // Обновление опций для выбора на основе полученных данных
+    this.typeOptions = rawData.types.map(type => type.type_name)
+    this.groupOptions = rawData.groups.map(group => group.group_name)
+    this.materialOptions = rawData.materials.map(material => material.mat_name)
   },
-  computed: {
-    popupTitle() {
+  computed: {  // Вычисляемые свойства
+    popupTitle() {  // Заголовок модального окна
+      // В зависимости от наличия id у инструмента выбирается текст заголовка
       return this.tool?.id != null
         ? `Редактировать инструмент ID: ${this.tool.id}`
         : 'Добавить инструмент'
     },
   },
-  methods: {
-    onCancel() {
-      this.$emit('canceled')
+  methods: {  // Методы компонента
+    onCancel() {  // Обработчик клика по кнопке "Закрыть"
+      this.$emit('canceled')  // Генерация пользовательского события "canceled"
     },
-    onSave() {
-      this.$emit('changes-saved', this.toolModel)
+    onSave() {  // Обработчик клика по кнопке "Сохранить"
+      this.$emit('changes-saved', this.toolModel)  // Генерация пользовательского события "changes-saved" с передачей данных инструмента
     },
   },
 }
 </script>
-
