ba3caa2444e8f2d13ee8b0cb2ec5010dcec9d50f
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2023-11-02 15:07:03 +0300

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,60 +1,61 @@
-const BASE_URL = 'http://localhost:4000';
+const BASE_URL = 'http://localhost:4000'
 
 // Получить все инструменты и связанные данные
 export async function fetchTools() {
   try {
-    const response = await fetch(`${BASE_URL}/tools`);
+    const response = await fetch(`${BASE_URL}/tools`)
     if (!response.ok) {
-      throw new Error('Network response was not ok ' + response.statusText);
+      throw new Error('Network response was not ok ' + response.statusText)
     }
-    const data = await response.json();
-    return data;
+    const data = await response.json()
+    return data
   } catch (error) {
-    console.error('There has been a problem with your fetch operation:', error);
+    console.error('There has been a problem with your fetch operation:', error)
   }
 }
 
 // Добавить новый инструмент
 export async function addTool(toolData) {
   try {
+    console.log("toolData")
+    console.log(toolData)
     const response = await fetch(`${BASE_URL}/add-tool`, {
       method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(toolData),
-    });
+    })
     if (!response.ok) {
-      throw new Error('Network response was not ok ' + response.statusText);
+      const text = await response.text()
+      throw new Error('Network response was not ok: ' + response.statusText + ', body: ' + text)
     }
-    const data = await response.json();
-    return data;
+    const data = await response.json()
+    return data
   } catch (error) {
-    console.error('There has been a problem with your fetch operation:', error);
-    return null;
+    console.error('There has been a problem with your fetch operation:', error)
+    return null
   }
 }
+
 
 // Обновить существующий инструмент
 export async function updateTool(id, toolData) {
   try {
     const response = await fetch(`${BASE_URL}/edit-tool/${id}`, {
       method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(toolData),
-    });
+    })
     if (!response.ok) {
-      throw new Error('Network response was not ok ' + response.statusText);
+      throw new Error('Network response was not ok ' + response.statusText)
     }
-    const data = await response.json();
-    return data;
+    const data = await response.json()
+    return data
   } catch (error) {
-    console.error('There has been a problem with your fetch operation:', error);
-    return null;
+    console.error('There has been a problem with your fetch operation:', error)
+    return null
   }
 }
+
 export async function addMaterial(name) {
   const response = await fetch('/add-material', {
     method: 'POST',
@@ -62,28 +63,24 @@ export async function addMaterial(name) {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({ name }),
-  });
-  return response.json();
+  })
+  return response.json()
 }
 
 export async function addType(name) {
   const response = await fetch('/add-type', {
     method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ name }),
-  });
-  return response.json();
+  })
+  return response.json()
 }
 
 export async function addGroup(name) {
   const response = await fetch('/add-group', {
     method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ name }),
-  });
-  return response.json();
+  })
+  return response.json()
 }
