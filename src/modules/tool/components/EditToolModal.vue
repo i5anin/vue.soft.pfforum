d794c1e3bf4f723f4ce4c51db48606ffa0cecde0
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2023-11-03 08:24:16 +0300

    Update EditToolModal.vue

--- src/modules/tool/components/EditToolModal.vue
+++ src/modules/tool/components/EditToolModal.vue
@@ -51,20 +51,10 @@ export default {
     tool: {
       type: Object,  // Тип свойства
       default: () => ({  // Значение по умолчанию
-        id: null,
-        group_name: '',
-        type_name: '',
-        mat_name: '',
-        name: '',
-        kolvo_sklad: 0,
-        norma: 0,
-        zakaz: 0,
-        rad: 0,
+        id: null, group_name: '', type_name: '', mat_name: '', name: '', kolvo_sklad: 0, norma: 0, zakaz: 0, rad: 0,
       }),
     },
-    radiusOptions: {
-      type: Array,  // Тип свойства
-    },
+    radiusOptions: { type: Array },// Тип свойства
   },
   components: { Modal }, // Список компонентов, используемых в данном компоненте
   data: () => ({  // Локальное состояние компонента
@@ -74,12 +64,7 @@ export default {
     materialOptions: [],  // Опции для выбора материала
   }),
   watch: {  // Слежка за изменением свойств и данных
-    tool: {
-      immediate: true,  // Немедленное выполнение при инициализации
-      handler(tool) {  // Обработчик изменения свойства
-        this.toolModel = JSON.parse(JSON.stringify(tool))  // Клонирование объекта инструмента
-      },
-    },
+    tool: { immediate: true },// Немедленное выполнение при инициализации
   },
   async created() {  // Хук жизненного цикла, вызывается при создании компонента
     const rawData = await fetchTools()  // Получение данных с сервера
