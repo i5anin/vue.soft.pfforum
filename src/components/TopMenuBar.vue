dedc012ff86907404953e25dd202b238990e8051
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2023-11-03 09:28:06 +0300

    Update TopMenuBar.vue

--- src/components/TopMenuBar.vue
+++ src/components/TopMenuBar.vue
@@ -9,10 +9,17 @@
       PF-FORUM APP
     </v-toolbar-title>
     <v-spacer />
-    <!-- Три кнопки справа -->
-    <v-btn icon>
+    <!-- Поле поиска не работает -->
+    <v-text-field class='red'
+      v-model="search"
+      label="Поиск"
+      v-if="showSearch"
+    />
+    <!-- Кнопка для отображения поля поиска -->
+    <v-btn icon @click="showSearch = !showSearch">
       <v-icon>mdi-magnify</v-icon>
     </v-btn>
+    <!-- Три кнопки справа -->
     <v-btn icon>
       <v-icon>mdi-fullscreen</v-icon>
     </v-btn>
@@ -24,11 +31,16 @@
     </v-btn>
   </v-app-bar>
 </template>
+
 <script setup>
 import { ref, defineEmits } from 'vue'
 
 const drawer = ref(false)
 const emit = defineEmits(['update:drawer'])
+
+// Добавьте эти строки для функции поиска
+const search = ref('')
+const showSearch = ref(false)  // Переменная для управления видимостью поля поиска
 </script>
 
 <style scoped>
