df6095ea57fa3ed53594d25def12c71fb5fd6109
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2023-11-03 14:41:39 +0300

    pm2

---
+++ backend/pm2.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apps: [
+    {
+      name: 'backend.soft.vue.pf-forum',
+      script: 'npm',
+      args: 'server'
+    }
+  ]
+};
---
+++ pm2.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apps: [
+    {
+      name: 'soft.vue.pf-forum',
+      script: 'npm',
+      args: 'start'
+    }
+  ]
+};
