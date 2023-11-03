b8edafb5e1936c4e58da874356a92551cacada13
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2023-11-03 15:59:40 +0300

    ttt

--- backend/pm2.config.js
+++ backend/pm2.config.js
@@ -2,8 +2,17 @@ module.exports = {
   apps: [
     {
       name: 'backend.soft.vue.pf-forum',
-      script: 'npm',
-      args: 'server'
+      script: 'backend/server.js',  // Указывает на ваш файл сервера
+      instances: 1,  // Запускает один экземпляр вашего приложения
+      autorestart: true,  // Автоматически перезапускает приложение при сбоях
+      watch: false,  // Отключает автоматический перезапуск при изменении файлов
+      max_memory_restart: '1G',  // Ограничивает использование памяти для перезапуска
+      env: {
+        NODE_ENV: 'development'  // Устанавливает переменные среды для разработки
+      },
+      env_production: {
+        NODE_ENV: 'production'  // Устанавливает переменные среды для продакшена
+      }
     }
   ]
 };
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,8 @@ module.exports = {
   apps: [
     {
       name: 'soft.vue.pf-forum',
-      script: 'vite',
-      args: '--host',
+      script: 'npm',
+      args: 'run serve',
       env: {
         PM2_PUBLIC_KEY: process.env.PM2_PUBLIC_KEY,
         PM2_SECRET_KEY: process.env.PM2_SECRET_KEY
@@ -12,6 +12,7 @@ module.exports = {
     {
       name: 'backend.soft.vue.pf-forum',
       script: 'backend/server.js',
+      args: 'run server',
       env: {
         PM2_PUBLIC_KEY: process.env.PM2_PUBLIC_KEY,
         PM2_SECRET_KEY: process.env.PM2_SECRET_KEY
