f5889de04b6eb4ad17e5f67e7702f2143332ac40
Isanin Sergey <sergey.isanin@gmail.com>
2023-11-19 21:30:21 +0300

    Update ToolLibraryTable.vue

--- src/views/Tools/ToolLibraryTable.vue
+++ src/views/Tools/ToolLibraryTable.vue
@@ -6,7 +6,7 @@
       </v-col>
     </v-row>
 
-    <v-simple-table>
+    <v-simple-table class="styled-table">
       <template v-slot:default>
         <thead>
         <tr>
@@ -61,3 +61,35 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+.styled-table {
+  border-collapse: collapse;
+  margin: 15px 0;
+  font-size: 0.9em;
+  min-width: 400px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+}
+.styled-table thead tr {
+  background-color: #005b98;
+  color: #ffffff;
+  text-align: left;
+}
+.styled-table th,
+.styled-table td {
+  padding: 12px 15px;
+}
+.styled-table tbody tr {
+  //border-bottom: 1px solid #005b98;
+}
+.styled-table tbody tr:nth-of-type(even) {
+  background-color: #f3f3f3;
+}
+.styled-table tbody tr:last-of-type {
+  border-bottom: 2px solid #005b98;
+}
+.styled-table tbody tr.active-row {
+  font-weight: bold;
+  color: #005b98;
+}
+</style>
