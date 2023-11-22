3f26af046083dd3cb6a1284e567fa81844d1666d
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2023-11-22 09:48:05 +0300

    auth

---
+++ src/modules/auth/pages/AccessErrorPage.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+  import { useLifecycleLogger } from "@/modules/common/hooks/lifecycleLogger";
+
+  const name = "AccessErrorPage";
+  useLifecycleLogger({ name });
+</script>
+
+<template>
+  <div class="page">Oops, you have no access!</div>
+</template>
+
+<style scoped></style>
