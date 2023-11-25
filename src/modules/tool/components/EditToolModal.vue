87f07afd51130718b2c4938a5299e7e111c9dd9e
Isanin Sergey <sergey.isanin@gmail.com>
2023-11-25 21:30:04 +0300

    +-

--- src/modules/tool/components/EditToolModal.vue
+++ src/modules/tool/components/EditToolModal.vue
@@ -1,84 +1,84 @@
 <template>
   <!--  <form @submit.prevent='onSubmit'>-->
-  <Modal :title='popupTitle'>
+  <Modal :title="popupTitle">
     <template #content>
       <v-container>
         <v-row>
-          <v-col class='flex'>
+          <v-col class="flex">
             <!--            левый столбец -->
             <div>
               <v-combobox
-                label='Название (Тип)'
-                v-model='toolModel.type'
-                :items='typeOptions'
-                item-text='text'
-                item-value='value'
+                label="Название (Тип)"
+                v-model="toolModel.type"
+                :items="typeOptions"
+                item-text="text"
+                item-value="value"
                 required
-                :counter='3'
-                :rules='typeRules'
+                :counter="3"
+                :rules="typeRules"
               />
               <v-combobox
-                label='Группа'
-                v-model='toolModel.group'
-                :items='groupOptions'
-                item-text='text'
-                item-value='value'
+                label="Группа"
+                v-model="toolModel.group"
+                :items="groupOptions"
+                item-text="text"
+                item-value="value"
                 required
-                :rules='typeRules'
+                :rules="typeRules"
               />
               <v-combobox
-                label='Применяемость материала'
-                v-model='toolModel.mat'
-                :items='materialOptions'
-                item-text='text'
-                item-value='value'
+                label="Применяемость материала"
+                v-model="toolModel.mat"
+                :items="materialOptions"
+                item-text="text"
+                item-value="value"
                 required
-                :rules='typeRules'
+                :rules="typeRules"
               />
 
               <v-combobox
-                label='Маркировка'
-                v-model='toolModel.name'
-                :items='nameOptions'
+                label="Маркировка"
+                v-model="toolModel.name"
+                :items="nameOptions"
                 required
-                :rules='typeRules'
+                :rules="typeRules"
               />
             </div>
-            <h2 class='text-h6'>Размеры:</h2>
+            <h2 class="text-h6">Размеры:</h2>
             <!-- правый столбец -->
             <div>
               <v-row>
-                <v-col cols='4'/>
-                <v-col cols='8'>
+                <v-col cols="4" />
+                <v-col cols="8">
                   <v-text-field
-                    label='Радиус (Пластины)'
-                    v-model='toolModel.radius'
+                    label="Радиус (Пластины)"
+                    v-model="toolModel.radius"
                     required
                   />
                   <v-text-field
-                    label='Диаметр (Сверла)'
-                    v-model='toolModel.diam'
+                    label="Диаметр (Сверла)"
+                    v-model="toolModel.diam"
                     required
                   />
                 </v-col>
               </v-row>
 
               <v-combobox
-                label='Шаг'
-                v-model='toolModel.shag'
-                :items='shagOptions'
+                label="Шаг"
+                v-model="toolModel.shag"
+                :items="shagOptions"
                 required
               />
               <v-combobox
-                label='Габариты'
-                v-model='toolModel.gabarit'
-                :items='gabaritOptions'
+                label="Габариты"
+                v-model="toolModel.gabarit"
+                :items="gabaritOptions"
                 required
               />
               <v-combobox
-                label='Вылет (Резцы)'
-                v-model='toolModel.width'
-                :items='widthOptions'
+                label="Вылет (Резцы)"
+                v-model="toolModel.width"
+                :items="widthOptions"
                 required
               />
             </div>
@@ -88,29 +88,29 @@
     </template>
     <template #action>
       <v-btn
-        color='red darken-1'
-        variant='text'
-        @click='confirmDelete'
-        class='text-none text-subtitle-1 ml-3'
+        color="red darken-1"
+        variant="text"
+        @click="confirmDelete"
+        class="text-none text-subtitle-1 ml-3"
       >
         Удалить
       </v-btn>
       <v-spacer />
       <v-btn
-        color='red darken-1'
-        variant='text'
-        @click='onCancel'
-        class='text-none text-subtitle-1 ml-3'
+        color="red darken-1"
+        variant="text"
+        @click="onCancel"
+        class="text-none text-subtitle-1 ml-3"
       >
         Закрыть
       </v-btn>
       <v-btn
-        prepend-icon='mdi-check-circle'
-        @click='onSave'
-        class='text-none text-subtitle-1 pl-3'
-        color='blue darken-1'
-        size='large'
-        variant='flat'
+        prepend-icon="mdi-check-circle"
+        @click="onSave"
+        class="text-none text-subtitle-1 pl-3"
+        color="blue darken-1"
+        size="large"
+        variant="flat"
       >
         Сохранить
       </v-btn>
@@ -118,28 +118,19 @@
   </Modal>
   <!--  </form> -->
   <DeleteConfirmationDialog
-    :confirmDeleteDialog='confirmDeleteDialog'
-    :onDelete='onDelete'
+    :confirmDeleteDialog="confirmDeleteDialog"
+    :onDelete="onDelete"
   />
 </template>
 
 <script>
 import Modal from '@/components/shared/Modal.vue'
-import {
-  addTool,
-  deleteTool,
-  updateTool,
-  getLibraries,
-  addMaterial,
-  addType,
-  addGroup,
-  getUniqueToolSpecs,
-} from '@/api'
 import DeleteConfirmationDialog from '@/modules/tool/components/DeleteConfirmationDialog.vue'
+import { mapState, mapActions } from 'vuex'
 
 export default {
   name: 'EditToolModal',
-  emits: ['canceled', 'changes-saved'],
+  components: { DeleteConfirmationDialog, Modal },
   props: {
     persistent: {
       type: Boolean,
@@ -153,18 +144,14 @@ export default {
         type_name: '',
         mat_name: '',
         name: '',
-        diam: '', // Переименовано из diam
+        diam: '',
         shag: '',
         typeOptions: ['Radius', 'Diam', 'Step', 'Dimensions', 'Projection'],
       }),
     },
     radiusOptions: { type: Array },
   },
-  components: { DeleteConfirmationDialog, Modal },
   data: () => ({
-    shagOptions: [],
-    gabaritOptions: [],
-    widthOptions: [],
     toolModel: {
       type: '',
       group: '',
@@ -172,6 +159,9 @@ export default {
       name: '',
       radius: '',
       diam: '',
+      shag: '',
+      gabarit: '',
+      width: '',
     },
     typeOptions: [],
     groupOptions: [],
@@ -181,60 +171,16 @@ export default {
     typeSelected: false,
     selectedType: '',
     typeRules: [
-      v => !!v || 'Поле обязательно для заполнения',
-      v => (v && v.length >= 3) || 'Минимальная длина: 3 символа',
+      (v) => !!v || 'Поле обязательно для заполнения',
+      (v) => (v && v.length >= 3) || 'Минимальная длина: 3 символа',
     ],
   }),
-
-  watch: {
-    tool: {
-      immediate: true,
-      handler(tool) {
-        const { mat, group, type } = tool
-        this.toolModel = {
-          ...tool,
-
-          mat: mat?.name === '[нет данных]' ? null : mat?.name,
-          group: group?.name === '[нет данных]' ? null : group?.name,
-          type: type?.name === '[нет данных]' ? null : type?.name,
-
-          radius: tool.spec?.radius,
-          shag: tool.spec?.shag,
-          gabarit: tool.spec?.gabarit,
-          width: tool.spec?.width,
-          diam: tool.spec?.diam, // Переименовано из diam
-        }
-        // console.log('Загрузка модели Tool Model:', this.toolModel) // Добавленный console.log
-      },
-    },
-  },
-  async mounted() {
-    // this.loadInitialData();
-    this.loadLastSavedData();
-    try {
-      const uniqueSpecs = await getUniqueToolSpecs()
-      this.shagOptions = uniqueSpecs.shags
-      this.gabaritOptions = uniqueSpecs.gabarits
-      this.widthOptions = uniqueSpecs.widths
-      this.nameOptions = uniqueSpecs.names // Заполняем опции маркировки
-    } catch (error) {
-      console.error('Ошибка при получении уникальных спецификаций:', error)
-    }
-
-
-    try {
-      const rawData = await getLibraries()
-      this.nameOptions = rawData.names.map((name) => ({ text: name, value: name }))
-
-      this.typeOptions = rawData.types.map((type) => type.name)
-      this.groupOptions = rawData.groups.map((group) => group.name)
-      this.materialOptions = rawData.materials.map((material) => material.name)
-
-    } catch (error) {
-      console.error('Ошибка при получении данных:', error)
-    }
-  },
   computed: {
+    ...mapState({
+      shagOptions: (state) => state.tool.shagOptions,
+      gabaritOptions: (state) => state.tool.gabaritOptions,
+      widthOptions: (state) => state.tool.widthOptions,
+    }),
     popupTitle() {
       return this.tool?.id != null
         ? `Редактировать инструмент ID: ${this.tool.id}`
@@ -242,47 +188,36 @@ export default {
     },
   },
   methods: {
+    ...mapActions('tool', [
+      'fetchShagOptions',
+      'fetchGabaritOptions',
+      'fetchWidthOptions',
+    ]),
     loadLastSavedData() {
-      const lastSavedData = localStorage.getItem('lastSavedToolModel');
+      const lastSavedData = localStorage.getItem('lastSavedToolModel')
       if (lastSavedData) {
-        const lastSavedToolModel = JSON.parse(lastSavedData);
-        this.prependLastSavedData(lastSavedToolModel);
+        const lastSavedToolModel = JSON.parse(lastSavedData)
+        this.prependLastSavedData(lastSavedToolModel)
       }
     },
-
     prependLastSavedData(data) {
-      this.prependOptionIfNeeded(data.type, this.typeOptions, 'type');
-      this.prependOptionIfNeeded(data.group, this.groupOptions, 'group');
-      this.prependOptionIfNeeded(data.mat, this.materialOptions, 'mat');
-
-      this.prependOptionIfNeeded(data.name, this.nameOptions, 'name');
-      // Для других полей, если они есть, добавьте по аналогии
+      this.prependOptionIfNeeded(data.type, this.typeOptions, 'type')
+      this.prependOptionIfNeeded(data.group, this.groupOptions, 'group')
+      this.prependOptionIfNeeded(data.mat, this.materialOptions, 'mat')
+      this.prependOptionIfNeeded(data.name, this.nameOptions, 'name')
     },
-
     prependOptionIfNeeded(value, optionsList, propName) {
-      console.log(`Value: ${value}`);
-      console.log(`OptionsList: ${JSON.stringify(optionsList)}`);
-
-      if (value && !optionsList.some(option => option.value === value)) {
-        const newOption = { text: `🔴 ${value}`, value: value };
-        optionsList.unshift(newOption);
+      if (value && !optionsList.some((option) => option.value === value)) {
+        const newOption = { text: `🔴 ${value}`, value: value }
+        optionsList.unshift(newOption)
       }
     },
-
-
     parseToFloat(value) {
       if (value === null) {
-        return 0 // Или другое значение по умолчанию
+        return 0
       }
       return parseFloat(value.toString().replace(',', '.'))
     },
-
-    // checkDisabledStatus() {
-    //   // console.log('Radius:', this.toolModel.radius)
-    //   // console.log('Diameter:', this.toolModel.diam)
-    //   return this.toolModel.radius || this.toolModel.diam
-    // },
-
     confirmDelete() {
       this.confirmDeleteDialog = true
     },
@@ -301,25 +236,15 @@ export default {
       this.$emit('canceled')
     },
     async onSave() {
-      const { id, group, type, mat, name } = this.toolModel;
+      const { id, group, type, mat, name } = this.toolModel
+      const { groups, materials, types } = await getLibraries()
+      let groupId = groups.find((g) => g.name === group)?.id
+      let matId = materials.find((m) => m.name === mat)?.id
+      let typeId = types.find((t) => t.name === type)?.id
 
-      const { groups, materials, types } = await getLibraries();
-      let groupId = groups.find(g => g.name === group)?.id;
-      let matId = materials.find(m => m.name === mat)?.id;
-      let typeId = types.find(t => t.name === type)?.id;
-
-      if (!groupId) {
-        const newGroup = await addGroup(group);
-        groupId = newGroup.id;
-      }
-      if (!matId) {
-        const newMaterial = await addMaterial(mat);
-        matId = newMaterial.id;
-      }
-      if (!typeId) {
-        const newType = await addType(type);
-        typeId = newType.id;
-      }
+      if (!groupId) groupId = (await addGroup(group)).id
+      if (!matId) matId = (await addMaterial(mat)).id
+      if (!typeId) typeId = (await addType(type)).id
 
       const toolData = {
         id,
@@ -332,27 +257,36 @@ export default {
         gabarit: this.toolModel.gabarit,
         width: this.toolModel.width,
         diam: this.toolModel.diam,
-      };
+      }
 
       try {
-        let result;
+        let result
         if (id == null) {
-          result = await addTool(toolData);
+          result = await addTool(toolData)
         } else {
-          result = await updateTool(id, toolData);
+          result = await updateTool(id, toolData)
         }
 
         if (result) {
-          this.$emit('changes-saved');
-          localStorage.setItem('lastSavedToolModel', JSON.stringify(this.toolModel));
-          console.log('Инструмент сохранен в localStorage');
+          this.$emit('changes-saved')
+          localStorage.setItem(
+            'lastSavedToolModel',
+            JSON.stringify(this.toolModel)
+          )
         }
       } catch (error) {
-        console.error('Ошибка при добавлении/обновлении инструмента:', error.message);
+        console.error(
+          'Ошибка при добавлении/обновлении инструмента:',
+          error.message
+        )
       }
     },
   },
+  async mounted() {
+    await this.fetchShagOptions()
+    await this.fetchGabaritOptions()
+    await this.fetchWidthOptions()
+    // Другие действия при монтировании
+  },
 }
 </script>
-
-
--- src/store/store.js
+++ src/store/store.js
@@ -1,17 +1,28 @@
 // src/store.js или src/store/index.js
-import { createStore } from 'vuex';
+import { createStore } from 'vuex'
 
-export default createStore({
+export default {
   state: {
-    // ваше состояние здесь
-  },
-  mutations: {
-    // ваши мутации здесь
-  },
-  actions: {
-    // ваши действия здесь
+    shagOptions: [],
+    gabaritOptions: [],
+    widthOptions: [],
+    // другие данные
   },
   getters: {
-    // ваши геттеры здесь
-  }
-});
+    shagOptions: (state) => state.shagOptions,
+    // другие геттеры
+  },
+  mutations: {
+    setShagOptions(state, options) {
+      state.shagOptions = options
+    },
+    // другие мутации
+  },
+  actions: {
+    async fetchShagOptions({ commit }) {
+      const uniqueSpecs = await getUniqueToolSpecs() // Предполагается, что getUniqueToolSpecs - это функция API
+      commit('setShagOptions', uniqueSpecs.shags)
+    },
+    // другие действия
+  },
+}
