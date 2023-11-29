107b7b0acb6dec45012b0ef425ca64bf32da6d39
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2023-11-29 14:41:21 +0300

    Update routers.js

--- backend/src/routers.js
+++ backend/src/routers.js
@@ -1,12 +1,14 @@
-const express = require('express');
-const routers = express.Router();
-const controllers = require('./controllers/tool');
-const login = require('./controllers/login');
-const { validateUser, getDatabaseInfo } = require('./controllers/login');
+const express = require('express')
+const routers = express.Router()
+const controllers = require('./controllers/tool')
+const toolTreeControllers = require('./controllers/tool_tree')
+
+const login = require('./controllers/login')
+const { validateUser, getDatabaseInfo } = require('./controllers/login')
 
 // Маршруты аутентификации
-routers.post('/validate-user', login.validateUser);
-routers.get('/database-info', getDatabaseInfo);
+routers.post('/validate-user', login.validateUser)
+routers.get('/database-info', getDatabaseInfo)
 
 // login, pass ---> access_token, refresh_token.
 // access_token 1 раз в 5 минут обновляется.
@@ -19,26 +21,28 @@ routers.get('/database-info', getDatabaseInfo);
 // hr - 3
 // Инструментальные маршруты
 // Основные сведения об инструменте
-routers.get('/tools', controllers.getTools);
-routers.get('/tool-library', controllers.getLibrary);
-routers.get('/tool-spec', controllers.getToolNomSpec);
-routers.get('/unique-tool-specs', controllers.getUniqueToolSpecs);
+routers.get('/tools', controllers.getTools)
+routers.get('/tool-library', controllers.getLibrary)
+routers.get('/tool-spec', controllers.getToolNomSpec)
+routers.get('/unique-tool-specs', controllers.getUniqueToolSpecs)
 
 // Инвентаризация и управление инструментами
-routers.get('/tools-inventory', controllers.getToolsWithInventoryInfo);
-routers.post('/tool', controllers.addTool);
-routers.put('/tool/:id', controllers.editTool);
-routers.delete('/tool/:id', controllers.deleteTool);
+routers.get('/tools-inventory', controllers.getToolsWithInventoryInfo)
+routers.post('/tool', controllers.addTool)
+routers.put('/tool/:id', controllers.editTool)
+routers.delete('/tool/:id', controllers.deleteTool)
 
 // Добавление инструмента на склад с параметрами zakaz, norma, kolvo_sklad
-routers.post('/add-to-warehouse/:id', controllers.addToWarehouse);
+routers.post('/add-to-warehouse/:id', controllers.addToWarehouse)
 
 // Дополнительные ресурсы, связанные с инструментами
-routers.post('/add-material', controllers.addMaterial);
-routers.post('/add-type', controllers.addType);
-routers.post('/add-group', controllers.addGroup);
-routers.delete('/material/:id', controllers.deleteMaterial);
-routers.delete('/type/:id', controllers.deleteType);
-routers.delete('/group/:id', controllers.deleteGroup);
+routers.post('/add-material', controllers.addMaterial)
+routers.post('/add-type', controllers.addType)
+routers.post('/add-group', controllers.addGroup)
+routers.delete('/material/:id', controllers.deleteMaterial)
+routers.delete('/type/:id', controllers.deleteType)
+routers.delete('/group/:id', controllers.deleteGroup)
 
-module.exports = routers;
+routers.get('/tools-tree', toolTreeControllers.getToolsTree)
+
+module.exports = routers
