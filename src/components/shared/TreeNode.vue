d26c811b81af290753bbaaee3293dd2a43ada924
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2023-11-30 08:46:59 +0300

    TreeNode

---
+++ src/components/shared/TreeNode.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-list-group
+    v-if="node.nodes && node.nodes.length"
+    :value="true"
+    no-action
+    sub-group
+  >
+    <template v-slot:activator>
+      <v-list-item-content>
+        <v-list-item-title>{{ node.label }}</v-list-item-title>
+      </v-list-item-content>
+    </template>
+    <v-list-item v-for="(child, index) in node.nodes" :key="index">
+      <tree-node :node="child"></tree-node>
+    </v-list-item>
+  </v-list-group>
+  <v-list-item v-else>
+    <v-list-item-content>
+      <v-list-item-title>{{ node.label }}</v-list-item-title>
+    </v-list-item-content>
+  </v-list-item>
+</template>
+
+<script>
+export default {
+  props: {
+    node: Object,
+  },
+}
+</script>
--- src/modules/tool/components/ToolTabTree.vue
+++ src/modules/tool/components/ToolTabTree.vue
@@ -1,28 +1,30 @@
 <template>
   <div>
-    <button @click="addNewNode">Add New Node</button>
-    <vue3-jstree v-if="treeData.nodes.length > 0" :data="treeData.nodes" />
-    <p v-else>No tree data available</p>
+    <v-btn @click="addNewNode">Add New Node</v-btn>
+    <v-list dense>
+      <template v-for="(item, index) in treeData" :key="index">
+        <tree-node :node="item"></tree-node>
+      </template>
+    </v-list>
+    <p v-if="!treeData.length">No tree data available</p>
   </div>
 </template>
 
 <script>
 import { ref, onMounted } from 'vue'
-import Vue3Jstree from '@ventralnet/vue3-jstree'
 import { getToolsTree } from '@/api'
+import TreeNode from '@/components/shared/TreeNode.vue'
 
 export default {
-  components: { Vue3Jstree },
+  components: { TreeNode },
   setup() {
-    const treeData = ref({ label: 'root', nodes: [] })
+    const treeData = ref([])
 
-    // Method to fetch the initial tree data
     const fetchTreeData = async () => {
       try {
         const data = await getToolsTree()
-        console.log('Fetched data:', data) // Add this line to log the fetched data
         if (Array.isArray(data)) {
-          treeData.value.nodes = data
+          treeData.value = data
         } else {
           console.error('Fetched data is not an array:', data)
         }
@@ -31,23 +33,9 @@ export default {
       }
     }
 
-    // Method to add a new node
     const addNewNode = () => {
-      console.log('Adding new node')
-      const newNode = {
-        label: 'New Node', // Label of the new node
-        nodes: [], // Empty nodes array
-      }
-
-      console.log('newNode init')
-
-      // Ensure treeData.nodes is an array before pushing
-      if (Array.isArray(treeData.value.nodes)) {
-        console.log(treeData)
-        treeData.value.nodes.push(newNode) // Adding the new node at the root level
-      } else {
-        console.error('treeData.nodes is not an array:', treeData.value.nodes)
-      }
+      const newNode = { label: 'New Node', nodes: [] }
+      treeData.value.push(newNode)
     }
 
     onMounted(fetchTreeData)
