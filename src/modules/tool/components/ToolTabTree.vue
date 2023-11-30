c9eccae7a9948d384af86db0b493b8faada00323
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2023-11-30 16:43:18 +0300

    Update ToolTabTree.vue

--- src/modules/tool/components/ToolTabTree.vue
+++ src/modules/tool/components/ToolTabTree.vue
@@ -8,10 +8,18 @@
       </v-btn>
     </v-app-bar>
     <v-main>
-      <v-container fluid>
+      <v-container :fluid="true">
         <v-row>
           <v-col cols="12">
-            <h1>{{ currentItem ? currentItem.label : '' }}</h1>
+            <div class="text-h6">
+              {{ currentItem ? currentItem.label : '' }}
+            </div>
+            <div class="breadcrumbs">
+              <span v-for="(item, index) in history" :key="index">
+                <span @click="goTo(index)">{{ item.label }}</span>
+                <span v-if="index < history.length - 1"> / </span>
+              </span>
+            </div>
             <v-list-item-group
               v-model="selectedItem"
               active-class="deep-purple--text text--accent-4"
@@ -22,7 +30,7 @@
                 @click="selectItem(item)"
               >
                 <v-list-item-content>
-                  <v-list-item-title v-text="item.label"></v-list-item-title>
+                  <v-list-item-title v-text="item.label" />
                 </v-list-item-content>
               </v-list-item>
             </v-list-item-group>
@@ -45,18 +53,29 @@ export default {
   }),
   methods: {
     selectItem(item) {
-      this.history.push(this.items)
-      this.items = item.nodes
+      if (this.currentItem) {
+        this.history.push(this.currentItem)
+      }
       this.currentItem = item
+      this.items = item.nodes
     },
     goBack() {
-      if (this.history.length > 0) {
-        this.items = this.history.pop()
+      if (this.history.length > 1) {
+        this.history.pop()
+        this.currentItem = this.history[this.history.length - 1]
+        this.items = this.currentItem.nodes
       }
+    },
+    goTo(index) {
+      this.history = this.history.slice(0, index + 1)
+      this.currentItem = this.history[index]
+      this.items = this.currentItem.nodes
     },
   },
   async created() {
     this.items = await getToolsTree()
+    this.currentItem = this.items[0]
+    this.items = this.currentItem.nodes
   },
 }
 </script>
