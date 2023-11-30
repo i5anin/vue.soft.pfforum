c8306c959c8fde7c4b7cf09f0efba0dad67d46ff
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2023-11-30 14:56:02 +0300

    Revert "Update TreeNode.vue"
    This reverts commit 40cb171611726f18dec25288bc04b8ab7b3fd78c.

--- src/modules/tool/components/TreeNode.vue
+++ src/modules/tool/components/TreeNode.vue
@@ -1,34 +1,25 @@
 <!-- TreeNode.vue -->
 <template>
-  <v-list-item @click="toggle">
-    <v-list-item-content>
-      <v-list-item-title v-text="node.label" />
-    </v-list-item-content>
-    <v-list-group
-      v-if="node.nodes && node.nodes.length > 0"
-      :value="open.includes(node.id)"
-      @input="toggle"
-    >
-      <template v-slot:activator="{ props }">
-        <v-list-item v-bind="props">
-          <v-list-item-content>
-            <v-list-item-title v-text="node.label" />
-          </v-list-item-content>
-        </v-list-item>
-      </template>
-      <v-list-item v-for="(child, index) in node.nodes" :key="index">
+  <v-list-group :value="open.includes(node.id)" @input="toggle">
+    <template v-slot:activator="{ props }">
+      <v-list-item v-bind="props" prepend-icon="mdi mdi-square-root">
         <v-list-item-content>
-          <v-list-item-title v-text="child.label" />
+          <v-list-item-title v-text="node.label" />
         </v-list-item-content>
       </v-list-item>
-      <tree-node
-        v-for="(child, index) in node.nodes"
-        :key="index"
-        :node="child"
-        :open.sync="open"
-      />
-    </v-list-group>
-  </v-list-item>
+    </template>
+    <v-list-item v-for="(child, index) in node.nodes" :key="index">
+      <v-list-item-content>
+        <v-list-item-title v-text="child.label" />
+      </v-list-item-content>
+    </v-list-item>
+    <tree-node
+      v-for="(child, index) in node.nodes"
+      :key="index"
+      :node="child"
+      :open.sync="open"
+    />
+  </v-list-group>
 </template>
 
 <script>
@@ -36,16 +27,16 @@ export default {
   name: 'TreeNode',
   props: ['node', 'open'],
   methods: {
-    toggle() {
-      if (this.open.includes(this.node.id)) {
+    toggle(value) {
+      if (value) {
+        this.open.push(this.node.id)
+        console.log(`Opened node: ${this.node.id}`)
+      } else {
         const index = this.open.indexOf(this.node.id)
         if (index > -1) {
           this.open.splice(index, 1)
         }
         console.log(`Closed node: ${this.node.id}`)
-      } else {
-        this.open.push(this.node.id)
-        console.log(`Opened node: ${this.node.id}`)
       }
     },
   },
