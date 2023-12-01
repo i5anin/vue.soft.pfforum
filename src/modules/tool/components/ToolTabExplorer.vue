d4958fd9f2d18b96de0467b8ada3f8183e6fbea1
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2023-12-01 09:59:38 +0300

    Update ToolTabExplorer.vue

--- src/modules/tool/components/ToolTabExplorer.vue
+++ src/modules/tool/components/ToolTabExplorer.vue
@@ -53,55 +53,33 @@ export default {
   }),
   methods: {
     selectItem(item) {
-      console.log('selectItem вызван с:', item)
       if (
         this.currentItem &&
         (!this.history.length ||
           this.currentItem.id !== this.history[this.history.length - 1].id)
       ) {
-        console.log(
-          'Текущий элемент до добавления в историю:',
-          this.currentItem
-        )
         this.history.push(this.currentItem)
-        console.log('История после добавления текущего элемента:', this.history)
       }
       this.currentItem = item
       this.items = item.nodes
-      console.log('Новый currentItem:', this.currentItem)
-      console.log('Новые items:', this.items)
     },
-
     goBack() {
-      console.log('goBack вызван')
       if (this.history.length > 1) {
-        console.log('История до изменения:', this.history)
         this.history.pop()
-        console.log('История после pop:', this.history)
         this.currentItem = this.history[this.history.length - 1]
         this.items = this.currentItem.nodes
-        console.log('Текущий currentItem после возврата:', this.currentItem)
-        console.log('Текущие items после возврата:', this.items)
       }
     },
     goTo(index) {
-      console.log('goTo вызван с индексом:', index)
-      console.log('История до обрезки:', this.history)
       this.history = this.history.slice(0, index + 1)
-      console.log('История после обрезки:', this.history)
       this.currentItem = this.history[index]
-      console.log('Текущий currentItem после goTo:', this.currentItem)
       this.items = this.currentItem.nodes
-      console.log('Текущие items после goTo:', this.items)
     },
   },
   async created() {
     this.items = await getToolsTree()
-    console.log('Начальные items:', this.items)
     this.currentItem = this.items[0]
-    console.log('Начальный currentItem:', this.currentItem)
     this.items = this.currentItem.nodes
-    console.log('Начальные nodes для currentItem:', this.items)
   },
 }
 </script>
