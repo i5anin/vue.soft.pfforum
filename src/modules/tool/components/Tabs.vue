29aec719cb87053a342312b37738f234cb27295c
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2023-12-04 15:01:37 +0300

    rename

--- src/modules/tool/components/Tabs.vue
+++ src/modules/tool/components/Tabs.vue
@@ -32,10 +32,10 @@
 <script setup>
 import { ref } from 'vue'
 
-import ToolTabTable from '@/modules/tool/components/MainTabTable.vue'
+import ToolTabTable from '@/modules/tool/components/TabMainTable.vue'
 import ToolTabLibrary from '@/modules/tool/components/TabLibrary.vue'
 import ToolTabInventory from '@/modules/tool/components/TabInventory.vue'
-import ToolTabTree from '@/modules/tool/components/ToolTabExplorer.vue'
+import ToolTabTree from '@/modules/tool/components/TabExplorer.vue'
 
 const tab = ref('Tree') // Установите начальное значение
 </script>
