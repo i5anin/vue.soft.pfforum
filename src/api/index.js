752c06a4b7f933f1f98f785aabb675605f0af638
Isanin Sergey <sergey.isanin@gmail.com>
2023-12-06 19:36:16 +0300

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,4 @@
-// import axios from 'axios'
-
-// const BASE_URL = import.meta.env.VITE_BASE_URL || 'http://localhost:4000/api'
-
-import { addTool, deleteTool } from './api_tool'
-
 export * from './api_login'
-
 export * from './api_tool'
 export * from './api_filter'
 export * from './api_libraries'
