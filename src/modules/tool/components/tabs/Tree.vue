4df2f9e27d8a5f6fe9bca47b1f80d9e77a759a9b
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2023-12-12 10:50:11 +0300

    Update Tree.vue

--- src/modules/tool/components/tabs/Tree.vue
+++ src/modules/tool/components/tabs/Tree.vue
@@ -2,9 +2,20 @@
   <v-app>
     <v-main>
       <v-container>
+        <!-- Кнопка обновления -->
+        <v-btn color="primary" @click="refreshTreeData">
+          <v-icon left>mdi-refresh</v-icon>
+          Обновить
+        </v-btn>
+
         <!-- Рекурсивное отображение дерева -->
-        <v-list-item-group>
-          <tree-node v-for="node in treeData" :key="node.id" :node="node" />
+        <v-list-item-group class="mt-3">
+          <tree-node
+            v-for="node in treeData"
+            :key="node.id"
+            :node="node"
+            @refresh-node="refreshTreeData"
+          />
         </v-list-item-group>
       </v-container>
     </v-main>
@@ -25,15 +36,21 @@ export default {
       treeData: [], // Инициализация пустого массива для данных дерева
     }
   },
+  methods: {
+    async refreshTreeData() {
+      try {
+        const updatedTree = await getTree()
+        if (updatedTree && updatedTree.length > 0) {
+          this.treeData = updatedTree
+        }
+      } catch (error) {
+        console.error('Ошибка при обновлении дерева:', error)
+        // Обработка ошибок или отображение сообщения пользователю
+      }
+    },
+  },
   async created() {
-    const toolsTree = await getTree()
-    if (toolsTree && toolsTree.length > 0) {
-      this.treeData = toolsTree
-    }
+    await this.refreshTreeData()
   },
 }
 </script>
-
-<style scoped>
-/* Стили для вашего дерева */
-</style>
