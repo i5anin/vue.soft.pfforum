b76c6d5b580a385e47427cf061332becf3386248
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2023-12-15 11:43:34 +0300

    Update Library.vue

--- src/modules/tool/components/tabs/Library.vue
+++ src/modules/tool/components/tabs/Library.vue
@@ -5,28 +5,28 @@
         <div class="flex">
           <h2 class="text-h5">Параметры Инструмента</h2>
           <v-spacer />
-          <v-btn color="primary" class="mb-2" @click="showAddDialog = true">
+          <v-btn color="primary" class="mb-2" @click="startAdding">
             Добавить параметр
           </v-btn>
         </div>
-        <v-dialog v-model="showAddDialog" persistent max-width="600px">
+        <v-dialog v-model="showDialog" persistent max-width="600px">
           <v-card>
-            <v-card-title>Новый Инструмент</v-card-title>
+            <v-card-title>{{ dialogTitle }}</v-card-title>
             <v-card-text>
               <v-text-field
                 label="Информация"
-                v-model="newParamInfo"
+                v-model="paramInfo"
                 required
               ></v-text-field>
             </v-card-text>
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn
-                lable="Отмена"
-                color="blue darken-1"
-                @click="showAddDialog = false"
-              />
-              <v-btn lable="Добавить" color="blue darken-1" @click="addParam" />
+              <v-btn color="blue darken-1" text @click="showDialog = false"
+                >Отмена</v-btn
+              >
+              <v-btn color="blue darken-1" text @click="saveParam"
+                >Сохранить</v-btn
+              >
             </v-card-actions>
           </v-card>
         </v-dialog>
@@ -40,11 +40,11 @@
             </tr>
           </thead>
           <tbody>
-            <tr v-for="(param, index) in toolParams" :key="param.id">
+            <tr v-for="param in toolParams" :key="param.id">
               <td>{{ param.id }}</td>
               <td>{{ param.info }}</td>
               <td>
-                <v-btn icon @click="renameParam(param)">
+                <v-btn icon @click="startEditing(param)">
                   <v-icon>mdi-pencil</v-icon>
                 </v-btn>
                 <v-btn icon @click="deleteParam(param)">
@@ -66,8 +66,10 @@ export default {
   data() {
     return {
       toolParams: [],
-      showAddDialog: false,
-      newParamInfo: '',
+      showDialog: false,
+      dialogTitle: '',
+      paramInfo: '',
+      editingParam: null,
     }
   },
   methods: {
@@ -78,25 +80,44 @@ export default {
         console.error('Error fetching data:', error)
       }
     },
-    renameParam(param) {
-      // Логика для переименования параметра
-      console.log(`Переименовать параметр: ${param.id}`)
+    startAdding() {
+      this.dialogTitle = 'Новый Инструмент'
+      this.paramInfo = ''
+      this.editingParam = null
+      this.showDialog = true
+    },
+    startEditing(param) {
+      this.dialogTitle = 'Редактировать Инструмент'
+      this.paramInfo = param.info
+      this.editingParam = param
+      this.showDialog = true
+    },
+    saveParam() {
+      if (this.paramInfo) {
+        if (this.editingParam) {
+          // Редактирование существующего параметра
+          this.editingParam.info = this.paramInfo
+        } else {
+          // Добавление нового параметра
+          const newParam = {
+            id: this.toolParams.length + 1, // или любой другой уникальный ID
+            info: this.paramInfo,
+          }
+          this.toolParams.push(newParam)
+        }
+        // Сброс формы после сохранения
+        this.paramInfo = ''
+        this.showDialog = false
+      } else {
+        alert('Пожалуйста, введите информацию для инструмента')
+      }
     },
     deleteParam(param) {
       if (confirm(`Вы уверены, что хотите удалить параметр: ${param.info}?`)) {
-        // Логика для удаления параметра
-        console.log(`Удалить параметр: ${param.id}`)
-      }
-    },
-    addParam() {
-      if (this.newParamInfo) {
-        // Добавьте здесь логику для добавления нового инструмента
-        console.log(`Добавить новый инструмент: ${this.newParamInfo}`)
-        // Сброс формы после добавления
-        this.newParamInfo = ''
-        this.showAddDialog = false
-      } else {
-        alert('Пожалуйста, введите информацию для нового инструмента')
+        const index = this.toolParams.indexOf(param)
+        if (index > -1) {
+          this.toolParams.splice(index, 1)
+        }
       }
     },
   },
