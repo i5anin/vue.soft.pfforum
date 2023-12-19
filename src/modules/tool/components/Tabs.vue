7ecd2fca21e08a38027b54a419fcc33518a5595f
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2023-12-19 16:15:07 +0300

    Update Tabs.vue

--- src/modules/tool/components/Tabs.vue
+++ src/modules/tool/components/Tabs.vue
@@ -1,25 +1,19 @@
 <template>
   <v-card>
     <v-tabs v-model="tab">
-      <v-tab value="Catalog"> Каталог </v-tab>
-      <v-tab value="Tree"> Дерево </v-tab>
-      <v-tab value="Param"> Параметры </v-tab>
-      <v-tab value="GiveTool"> Получить инструмент </v-tab>
+      <v-tab v-for="item in tabs" :key="item.component" :value="item.component">
+        {{ item.name }}
+      </v-tab>
     </v-tabs>
 
     <v-card-text>
       <v-window v-model="tab">
-        <v-window-item value="Catalog">
-          <TabCatalog :parentId="parentId" />
-        </v-window-item>
-        <v-window-item value="Tree">
-          <ToolTabTree />
-        </v-window-item>
-        <v-window-item value="Param">
-          <ToolTabParam />
-        </v-window-item>
-        <v-window-item value="GiveTool">
-          <GiveTool />
+        <v-window-item
+          v-for="item in tabs"
+          :key="item.component"
+          :value="item.component"
+        >
+          <component :is="item.component" :parentId="parentId" />
         </v-window-item>
       </v-window>
     </v-card-text>
@@ -28,24 +22,21 @@
 
 <script setup>
 import { ref } from 'vue'
-
 import ToolTabParam from '@/modules/tool/components/tabs/Param.vue'
 import GiveTool from '@/modules/tool/components/tabs/GiveTool.vue'
 import TabCatalog from '@/modules/tool/components/tabs/Catalog.vue'
 import ToolTabTree from '@/modules/tool/components/tabs/Tree.vue'
 
-// Получаем parentId как prop
 const props = defineProps({
   parentId: String,
 })
 
-const tab = ref('Catalog') // Начальное значение
-</script>
+const tab = ref('Catalog')
 
-<script>
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'TabsTools',
-})
+const tabs = [
+  { name: 'Каталог', component: TabCatalog },
+  { name: 'Дерево', component: ToolTabTree },
+  { name: 'Параметры', component: ToolTabParam },
+  { name: 'Получить инструмент', component: GiveTool },
+]
 </script>
