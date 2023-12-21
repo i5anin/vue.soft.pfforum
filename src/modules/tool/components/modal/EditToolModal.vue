f0deba65aa5789e7e5629e010dab16ffd9359279
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2023-12-21 08:37:44 +0300

    methods: ...mapState('tool', ['idParent']),

--- src/modules/tool/components/modal/EditToolModal.vue
+++ src/modules/tool/components/modal/EditToolModal.vue
@@ -213,7 +213,6 @@ export default {
     )
   },
   methods: {
-    ...mapState('tool', ['idParent']), // !!! TODO: ПОЛУЧИТЬ ДАННЫЕ ИЗ VUEX
     ...mapMutations('tool', ['setTool']),
     ...mapActions('tool', [
       'fetchUniqueToolSpecs',
