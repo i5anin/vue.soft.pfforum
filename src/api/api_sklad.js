9409a3ffad6db19b216613c41aa9b7ee38b0e57e
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2023-12-22 15:04:40 +0300

    Update api_sklad.js

--- src/api/api_sklad.js
+++ src/api/api_sklad.js
@@ -1,17 +1,13 @@
 import axios from 'axios'
 
 const BASE_URL = import.meta.env.VITE_BASE_URL || 'http://localhost:4000/api'
-export async function addSklad(toolId, kolvo_sklad, norma, zakaz) {
+export async function fetchToolHistory() {
   try {
-    const response = await axios.post(
-      `${BASE_URL}/add-to-warehouse/${toolId}`,
-      { kolvo_sklad, norma, zakaz }
-    )
+    const response = await axios.get(`${BASE_URL}/sklad`)
     console.log('Ответ сервера:', response)
     return response.data
   } catch (error) {
     console.error('Проблема с запросом на сервер:', error)
-    console.log(error.response) // Добавьте это для вывода ответа сервера
     return null
   }
 }
