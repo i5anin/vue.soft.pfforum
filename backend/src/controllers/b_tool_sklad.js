5b1f43889021a584637c246ce6db4eb960491d16
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2023-12-22 13:23:31 +0300

    Update b_tool_sklad.js

--- backend/src/controllers/b_tool_sklad.js
+++ backend/src/controllers/b_tool_sklad.js
@@ -11,202 +11,71 @@ const dbConfig =
 // Создание пула подключений к БД
 const pool = new Pool(dbConfig)
 
-async function getToolNomSpec(req, res) {
+async function getToolHistory(req, res) {
   try {
-    const specQuery = `
-      SELECT
-        id,
-        rad,
-        shag,
-        gab
-      FROM dbo.tool_nom_spec
-    `
-
-    const result = await pool.query(specQuery)
-
-    if (result.rows.length > 0) {
-      res.json(result.rows)
-    } else {
-      res.status(404).send('Спецификации не найдены.')
-    }
-  } catch (err) {
-    console.error('Error:', err.message)
-    console.error('Stack:', err.stack)
-    res.status(500).send(err.message)
-  }
-}
-
-async function getUniqueToolSpecs(req, res) {
-  try {
-    // Отдельные запросы для каждого поля с исключением пустых строк
-    const nameQuery = "SELECT DISTINCT name FROM dbo.tool_nom WHERE name <> ''"
-    const widthQuery =
-      "SELECT DISTINCT width FROM dbo.tool_nom WHERE width <> ''"
-    const gabaritQuery =
-      "SELECT DISTINCT gabarit FROM dbo.tool_nom WHERE gabarit <> ''"
-    const shagQuery = "SELECT DISTINCT shag FROM dbo.tool_nom WHERE shag <> ''"
-    const diamQuery = "SELECT DISTINCT diam FROM dbo.tool_nom WHERE diam <> ''"
-
-    // Выполнение запросов параллельно
-    const [names, widths, gabarits, shags, diams] = await Promise.all([
-      pool.query(nameQuery),
-      pool.query(widthQuery),
-      pool.query(gabaritQuery),
-      pool.query(shagQuery),
-      pool.query(diamQuery),
-    ])
-
-    // Объединение результатов
-    const result = {
-      names: names.rows.map((row) => row.name),
-      // widths: widths.rows.map((row) => row.width),
-      // gabarits: gabarits.rows.map((row) => row.gabarit),
-      // shags: shags.rows.map((row) => row.shag),
-      // diams: diams.rows.map((row) => row.diam),
-    }
-
-    res.json(result)
-  } catch (err) {
-    console.error('Error:', err.message)
-    console.error('Stack:', err.stack)
-    res.status(500).send(err.message)
-  }
-}
-
-// Определение контроллеров
-
-async function deleteTool(req, res) {
-  const { id } = req.params
-  try {
-    await pool.query(
-      `DELETE
-       FROM dbo.tool_nom
-       WHERE id = $1`,
-      [id]
-    )
-    res.json({ result: true })
-  } catch (error) {
-    console.error(error)
-    res.status(500).send(error.message)
-  }
-}
-
-async function addTool(req, res) {
-  const { name, property } = req.body
-
-  try {
-    // Преобразование объекта property в строку JSON для хранения в базе данных
-    const propertyString = JSON.stringify(property)
-
-    // Вставка данных инструмента
-    const toolInsertResult = await pool.query(
-      'INSERT INTO dbo.tool_nom (name, property) VALUES ($1, $2) RETURNING id',
-      [name, propertyString]
-    )
-
-    const toolId = toolInsertResult.rows[0].id
-
-    // Дополнительная логика обработки параметров, если необходимо
-    // ...
-
-    res.json({ toolId })
-  } catch (err) {
-    console.error(err)
-    res.status(500).send(err.message)
-  }
-}
-
-async function editTool(req, res) {
-  const { id } = req.params
-  const { name, group_id, mat_id, type_id, kolvo_sklad, norma, zakaz } =
-    req.body
-
-  try {
-    const result = await pool.query(
-      'UPDATE dbo.tool_nom SET name=$1, group_id=$2, mat_id=$3, type_id=$4, kolvo_sklad=$5, norma=$6, zakaz=$7, radius=$8, shag=$9, gabarit=$10, width=$11, diam=$12, geometry=$13 WHERE id=$14 RETURNING *',
-      [name, group_id, mat_id, type_id, kolvo_sklad, norma, zakaz, id] // Add geometry here
-    )
-
-    if (result.rowCount > 0) {
-      res.json({
-        message: 'Инструмент успешно обновлен.',
-        tool: result.rows[0],
-      })
-    } else {
-      res.status(404).send('Инструмент с указанным ID не найден.')
-    }
-  } catch (err) {
-    console.error('Error:', err.message)
-    console.error('Stack:', err.stack)
-    res.status(500).send(err.message)
-  }
-}
-
-//sklad
-async function getToolsWithInventoryInfo(req, res) {
-  try {
-    // Получение параметров запроса
-    const { search } = req.query
+    const { search, parent_id, includeNull } = req.query
     const page = parseInt(req.query.page || 1, 10)
     const limit = parseInt(req.query.limit || 15, 10)
     const offset = (page - 1) * limit
 
-    // Подготовка параметров для запросов
-    const searchCondition = search ? `WHERE tool_nom.name ILIKE $1` : ''
-    const limitOffsetCondition = search
-      ? `LIMIT $2::bigint OFFSET $3::bigint`
-      : `LIMIT $1::bigint OFFSET $2::bigint`
-    const queryParams = search
-      ? [`%${search}%`, limit, offset]
-      : [limit, offset]
+    let conditions = []
 
-    // Запрос на получение инструментов
-    const toolQuery = `
-      SELECT tool_nom.id,
-             tool_nom.name,
-             tool_nom.group_id,
-             tool_nom.mat_id,
-             tool_nom.type_id,
-             COALESCE (tool_group.name, '[нет данных]') as group_name,
-             COALESCE (tool_mat.name, '[нет данных]') as mat_name,
-             COALESCE (tool_type.name, '[нет данных]') as type_name,
-             tool_nom.kolvo_sklad,
-             tool_nom.norma,
-             tool_nom.zakaz
-      FROM dbo.tool_nom as tool_nom
-             LEFT JOIN dbo.tool_group as tool_group ON tool_nom.group_id = tool_group.id
-             LEFT JOIN dbo.tool_mat as tool_mat ON tool_nom.mat_id = tool_mat.id
-             LEFT JOIN dbo.tool_type as tool_type ON tool_nom.type_id = tool_type.id
-             ${searchCondition}
-      ORDER BY tool_nom.id DESC
-      ${limitOffsetCondition}
-    `
+    if (search) {
+      conditions.push(`tool_nom.name ILIKE '%${search.replace(/'/g, "''")}%'`)
+    }
 
-    // Запрос на получение общего количества инструментов
+    if (parent_id) {
+      conditions.push(`tool_nom.parent_id = ${parent_id}`)
+    }
+
+    if (!includeNull || includeNull === 'false') {
+      conditions.push(
+        `(tool_nom.name IS NOT NULL AND tool_nom.name != '' AND tool_nom.property IS NOT NULL)`
+      )
+    }
+
+    const whereClause = conditions.length
+      ? `WHERE ${conditions.join(' AND ')}`
+      : ''
+
     const countQuery = `
-      SELECT COUNT(*)::INTEGER
-      FROM dbo.tool_nom ${searchCondition}
+      SELECT COUNT(*)
+      FROM dbo.tool_history_nom
+      INNER JOIN dbo.tool_nom ON tool_history_nom.id_nom = tool_nom.id
+      ${whereClause}
     `
 
-    // Выполнение запросов
-    const [countResult, tools] = await Promise.all([
-      pool.query(countQuery, search ? [`%${search}%`] : []),
-      pool.query(toolQuery, queryParams),
+    const historyQuery = `
+      SELECT tool_history_nom.id_oper,
+             tool_history_nom.id_user,
+             tool_history_nom.id_nom,
+             tool_nom.name,
+             tool_history_nom.quantity,
+             tool_history_nom.date_p,
+             tool_history_nom.date_u
+      FROM dbo.tool_history_nom
+      INNER JOIN dbo.tool_nom ON tool_history_nom.id_nom = tool_nom.id
+      ${whereClause}
+      ORDER BY tool_history_nom.id_oper DESC
+      LIMIT ${limit} OFFSET ${offset}
+    `
+
+    const [countResult, historyResult] = await Promise.all([
+      pool.query(countQuery),
+      pool.query(historyQuery),
     ])
 
-    const totalCount = countResult.rows[0].count
+    const totalCount = parseInt(countResult.rows[0].count, 10)
 
-    // Форматирование данных инструментов
-    const formattedTools = tools.rows.map((tool) => {
+    const formattedHistory = historyResult.rows.map((history) => {
       return {
-        id: tool.id,
-        name: tool.name,
-        kolvo_sklad: tool.kolvo_sklad,
-        norma: tool.norma,
-        zakaz: tool.zakaz,
-        mat: { name: tool.mat_name, id: tool.mat_id },
-        type: { name: tool.type_name, id: tool.type_id },
-        group: { name: tool.group_name, id: tool.group_id },
+        id_oper: history.id_oper,
+        id_user: history.id_user,
+        id_nom: history.id_nom,
+        name: history.name,
+        quantity: history.quantity,
+        date_p: history.date_p,
+        date_u: history.date_u,
       }
     })
 
@@ -214,7 +83,7 @@ async function getToolsWithInventoryInfo(req, res) {
       currentPage: page,
       itemsPerPage: limit,
       totalCount,
-      tools: formattedTools,
+      history: formattedHistory,
     })
   } catch (err) {
     console.error(err)
@@ -222,34 +91,7 @@ async function getToolsWithInventoryInfo(req, res) {
   }
 }
 
-async function addToWarehouse(req, res) {
-  const { id } = req.params // ID инструмента
-  const { zakaz, norma, kolvo_sklad } = req.body // Полученные значения
-
-  try {
-    const result = await pool.query(
-      'UPDATE dbo.tool_nom SET zakaz=$1, norma=$2, kolvo_sklad=$3 WHERE id=$4 RETURNING *',
-      [zakaz, norma, kolvo_sklad, id] // Порядок параметров в соответствии с SQL-запросом
-    )
-
-    console.log(result)
-    if (result.rowCount > 0) {
-      res.json({
-        message: 'Инструмент успешно добавлен на склад.',
-        tool: result.rows[0],
-      })
-    } else {
-      res.status(404).send('Инструмент с указанным ID не найден.')
-    }
-  } catch (err) {
-    console.error('Error:', err.message)
-    console.error('Stack:', err.stack)
-    res.status(500).send(err.message)
-  }
-}
-
 // Экспорт контроллеров
 module.exports = {
-  addToWarehouse,
-  getToolsWithInventoryInfo,
+  getToolHistory,
 }
