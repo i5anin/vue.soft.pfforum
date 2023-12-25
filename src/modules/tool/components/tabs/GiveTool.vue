ca03995a92cf3a660f3ef1649799c1c541df6abc
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2023-12-25 14:26:51 +0300

    Update GiveTool.vue

--- src/modules/tool/components/tabs/GiveTool.vue
+++ src/modules/tool/components/tabs/GiveTool.vue
@@ -11,12 +11,6 @@
       :items="toolsHistory"
       :items-per-page-options="[15, 50, 100, 300]"
     >
-      <template v-slot:item.name="{ item }">
-        <span style="white-space: nowrap">{{ item.name }}</span>
-      </template>
-      <template v-slot:item.description="{ item }">
-        <span style="white-space: nowrap">{{ item.description }}</span>
-      </template>
     </v-data-table-server>
   </v-container>
 </template>
@@ -33,14 +27,14 @@ export default {
       totalCount: 0,
       headers: [
         { title: 'Название', key: 'name', sortable: true },
-        { title: 'description', key: 'description', sortable: true },
-        { title: 'no_oper', key: 'no_oper', sortable: true },
-        { title: 'type_oper', key: 'type_oper', sortable: true },
-        { title: 'quantity', key: 'quantity', sortable: true },
+        { title: 'Обозначение', key: 'description', sortable: true },
+        { title: 'Номер операции', key: 'no_oper', sortable: true },
+        { title: 'Тип операции', key: 'type_oper', sortable: true },
+        { title: 'Количество', key: 'quantity', sortable: true },
         { title: 'user_fio', key: 'user_fio', sortable: true },
-        { title: 'date_p', key: 'date_p', sortable: true },
-        { title: 'date_u', key: 'date_u', sortable: true },
-        { title: 'name_tool', key: 'name_tool', sortable: true },
+        { title: 'Дата приход', key: 'date_p', sortable: true },
+        { title: 'Дата уход', key: 'date_u', sortable: true },
+        { title: 'Название инструмента', key: 'name_tool', sortable: true },
         { title: 'Характеристики', key: 'property', sortable: true },
       ],
     }
