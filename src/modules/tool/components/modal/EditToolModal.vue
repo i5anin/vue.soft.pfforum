77541ea7366db80653b64dfb618eb74c13f19fa7
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2023-12-27 10:16:22 +0300

    Revert "Update EditToolModal.vue"
    This reverts commit 39fe9ac559b4692852595e99f574502a965a67c8.

--- src/modules/tool/components/modal/EditToolModal.vue
+++ src/modules/tool/components/modal/EditToolModal.vue
@@ -1,4 +1,5 @@
 <template>
+  <!--  <form @submit.prevent='onSubmit'>-->
   <Modal :title="popupTitle">
     <template #content>
       <v-container>
@@ -19,7 +20,7 @@
                 :disabled="true"
               />
             </div>
-
+            <!--левый столбец -->
             <div>
               <v-combobox
                 density="compact"
@@ -32,7 +33,7 @@
                 :rules="typeRules"
               />
             </div>
-
+            <!-- правый столбец -->
             <v-combobox
               :chips="true"
               multiple
@@ -41,8 +42,8 @@
               label="Параметры"
               return-object
             />
-
             <h2 class="text-h6">Характеристики:</h2>
+            <!-- динамические параметры -->
             <div v-for="param in selectedParamsInfo" :key="param.id">
               <v-combobox
                 density="compact"
@@ -52,7 +53,6 @@
                 required
               />
             </div>
-
             <h2 class="text-h6">Склад:</h2>
             <v-text-field
               density="compact"
@@ -66,17 +66,28 @@
               required
               v-model="toolModel.sklad"
             />
+            <h2 class="text-h6"></h2>
+            <div></div>
           </v-col>
         </v-row>
       </v-container>
     </template>
-
     <template #action>
-      <v-btn color="red darken-1" variant="text" @click="confirmDelete">
+      <v-btn
+        color="red darken-1"
+        variant="text"
+        @click="confirmDelete"
+        class="text-none text-subtitle-1 ml-3"
+      >
         Удалить
       </v-btn>
       <v-spacer />
-      <v-btn color="red darken-1" variant="text" @click="onCancel">
+      <v-btn
+        color="red darken-1"
+        variant="text"
+        @click="onCancel"
+        class="text-none text-subtitle-1 ml-3"
+      >
         Закрыть
       </v-btn>
       <v-btn
@@ -91,7 +102,7 @@
       </v-btn>
     </template>
   </Modal>
-
+  <!--  </form> -->
   <DeleteConfirmationDialog
     :confirmDeleteDialog="confirmDeleteDialog"
     :onDelete="onDelete"
@@ -106,82 +117,76 @@ import { mapActions, mapGetters, mapMutations, mapState } from 'vuex'
 
 export default {
   name: 'EditToolModal',
-  emits: ['canceled', 'changes-saved'],
+  emits: ['canceled', 'changes-saved'], // объявления пользовательских событий
+  //props контракт общение что использовать и что передавать от родительского компонента к дочернему
   props: {
     persistent: { type: Boolean, default: false },
     toolId: { type: Number, default: null },
     radiusOptions: { type: Array },
   },
   components: { DeleteConfirmationDialog, Modal },
-  data() {
-    return {
-      localParentId: null,
-      toolModel: { name: null, property: {} },
-      toolParamOptions: [],
-      selectedParams: [],
-      geometryOptions: [],
-      toolParams: [],
-      confirmDeleteDialog: false,
-      typeSelected: false,
-      selectedType: '',
-      parentIdRules: [
-        (v) => !!v || 'ID папки обязательно',
-        (v) => v > 1 || 'ID папки должен быть больше 1',
-        (v) => v !== '' || 'ID папки не должен быть пустым',
-      ],
-      typeRules: [
-        (v) => !!v || 'Поле обязательно для заполнения',
-        (v) => (v && v.length >= 3) || 'Минимальная длина: 3 символа',
-      ],
-    }
-  },
+  //реактивные данные
+  data: () => ({
+    localParentId: null,
+    toolModel: { name: null, property: {} },
+    toolParamOptions: [],
+    selectedParams: [],
+    geometryOptions: [],
+    toolParams: [],
+    confirmDeleteDialog: false,
+    typeSelected: false,
+    selectedType: '',
+    parentIdRules: [
+      (v) => !!v || 'ID папки обязательно',
+      (v) => v > 1 || 'ID папки должен быть больше 1',
+      (v) => v !== '' || 'ID папки не должен быть пустым',
+    ],
+    typeRules: [
+      (v) => !!v || 'Поле обязательно для заполнения',
+      (v) => (v && v.length >= 3) || 'Минимальная длина: 3 символа',
+    ],
+  }),
+  // [data] - используется для определения реактивных данных компонента, которые непосредственно управляют состоянием и поведением этого компонента.
+  // [watch] - используется для отслеживания изменений в этих данных (или в других реактивных источниках) и выполнения дополнительных действий или логики в ответ на эти изменения.
+
   watch: {
-    toolId(newVal) {
-      if (newVal) {
-        this.fetchToolById(newVal)
-      } else {
-        this.initializeState()
-      }
+    tool: {
+      deep: true,
+      immediate: true,
+      handler(newTool) {
+        if (newTool) {
+          this.localParentId = newTool.parent_id
+          this.currentFolderName = newTool.folder_name
+        } else {
+          this.localParentId = this.idParent.id
+          this.currentFolderName = this.idParent.label
+        }
+      },
     },
   },
-  mounted() {
-    this.initializeState()
-  },
-  methods: {
-    ...mapMutations('tool', ['setTool']),
-    ...mapActions('tool', [
-      'fetchUniqueToolSpecs',
-      'fetchToolsByFilter',
-      'onSaveToolModel',
-      'fetchToolById',
-    ]),
-    initializeState() {
-      this.localParentId = null
-      this.toolModel = { name: null, property: {} }
-      this.selectedParams = []
-      this.toolParamOptions = []
-      // Дополнительная инициализация по необходимости
-      this.fetchToolParams()
-    },
-    fetchToolParams() {
-      // Логика получения параметров инструмента
-      getToolParams().then((params) => {
-        this.toolParamOptions = params.map((param) => param.info)
+
+  async created() {
+    this.initializeLocalState()
+    if (this.toolId == null) {
+      this.setTool({
+        id: null,
+        name: null,
+        property: {},
       })
-    },
-    confirmDelete() {
-      this.confirmDeleteDialog = true
-    },
-    onCancel() {
-      this.$emit('canceled')
-    },
-    onSave() {
-      // Логика сохранения
-    },
-    onDelete() {
-      // Логика удаления
-    },
+    } else {
+      await this.fetchToolById(this.toolId)
+      if (this.tool.property === null) this.tool.property = {}
+    }
+    const rawToolParams = await getToolParams()
+    this.toolParams = [...rawToolParams]
+    this.toolModel = JSON.parse(JSON.stringify(this.tool))
+    const propertyIds = Object.keys(this.toolModel.property).map((key) => key)
+    this.selectedParams = this.toolParams
+      .filter(({ id }) => propertyIds.includes(String(id)))
+      .map(({ info }) => info)
+    this.toolParamOptions = rawToolParams.map((param) => param.info)
   },
+
   computed: {
     ...mapGetters('tool', [
       'widthOptions',
@@ -191,6 +196,12 @@ export default {
       'tool',
     ]),
     ...mapState('tool', ['idParent']),
+    currentParentId() {
+      return this.toolId === null ? this.idParent.id : this.tool.parent_id
+    },
+    currentFolderName() {
+      return this.toolId === null ? this.idParent.label : this.tool.folder_name
+    },
     selectedParamsInfo() {
       return this.selectedParams
         .map((paramName) =>
@@ -198,10 +209,99 @@ export default {
         )
         .filter((selectedParam) => selectedParam != null)
     },
+
     popupTitle() {
       return this.tool?.id != null
         ? `Редактировать инструмент ID: ${this.tool.id}`
         : 'Добавить инструмент'
+    },
+  },
+  methods: {
+    ...mapMutations('tool', ['setTool']),
+    ...mapActions('tool', [
+      'fetchUniqueToolSpecs',
+      'fetchToolsByFilter',
+      'onSaveToolModel',
+      'fetchToolById',
+    ]),
+    initializeLocalState() {
+      if (this.toolId) {
+        this.fetchToolById(this.toolId).then(() => {
+          this.toolModel.sklad = this.tool.sklad
+          this.toolModel.norma = this.tool.norma
+        })
+      } else {
+        this.localParentId = this.idParent.id
+        this.currentFolderName = this.idParent.label
+      }
+    },
+    logModelValue(paramId) {
+      console.log('Value changed for param ID:', paramId)
+    },
+
+    prependLastSavedData(data) {
+      if (!data) return
+      this.prependOptionIfNeeded(data.name, this.nameOptions, 'name')
+    },
+
+    prependOptionIfNeeded(value, optionsList) {
+      if (value && !optionsList.some((option) => option.value === value))
+        optionsList.unshift(value)
+    },
+
+    parseToFloat(value) {
+      if (value === null) return 0
+      return parseFloat(value.toString().replace(',', '.'))
+    },
+
+    confirmDelete() {
+      this.confirmDeleteDialog = true
+    },
+    async onDelete() {
+      const { id } = this.toolModel
+      if (id != null) {
+        try {
+          const { result } = await deleteTool(id)
+          if (result) {
+            this.$emit('changes-saved')
+            this.fetchToolsByFilter()
+          }
+        } catch (error) {
+          console.error('Ошибка при удалении инструмента:', error)
+        }
+      }
+    },
+    onCancel() {
+      this.$emit('canceled')
+    },
+    async onSave() {
+      const toolDataToSend = {
+        ...this.toolModel,
+        parent_id: this.localParentId,
+      }
+
+      try {
+        let response
+        if (this.toolId) {
+          response = await updateTool(this.toolId, toolDataToSend)
+        } else {
+          response = await addTool(toolDataToSend)
+        }
+        console.log(response, response.status)
+        if (response.success === 'OK') {
+          this.$emit('changes-saved')
+          this.fetchToolsByFilter()
+        } else {
+          console.error('Ошибка при сохранении: ', response.data)
+          // Оставляем форму открытой для всех ошибок, кроме 200
+        }
+      } catch (error) {
+        console.error(
+          'Ошибка при сохранении: ',
+          error.response ? error.response.data : error
+        )
+        // Оставляем форму открытой для всех ошибок
+      }
     },
   },
 }
