c1cd8161d7462fa9a2954ae5da7905509b7a055b
Isanin Sergey <sergey.isanin@gmail.com>
2024-01-06 21:16:43 +0300

    --

--- src/modules/tool/components/Tabs.vue
+++ src/modules/tool/components/Tabs.vue
@@ -1,48 +1,47 @@
 <template>
   <v-card>
     <v-tabs v-model="tab">
-      <v-tab v-for="item in tabs" :key="item.name" :value="item.name">
+      <v-tab
+        v-for="item in tabs"
+        :key="item.name"
+        @click="navigateToTab(item.routeName)"
+      >
         {{ item.name }}
       </v-tab>
     </v-tabs>
 
     <v-card-text>
-      <v-window v-model="tab">
-        <v-window-item v-for="item in tabs" :key="item.name" :value="item.name">
-          <component
-            :is="item.component"
-            :parentId="parentId"
-            :type="item.type"
-          />
-        </v-window-item>
-      </v-window>
+      <router-view />
     </v-card-text>
   </v-card>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import ToolTabParam from '@/modules/tool/components/tabs/Param.vue'
-import GiveTool from '@/modules/tool/components/tabs/GiveTool.vue'
-import TabCatalog from '@/modules/tool/components/tabs/Catalog.vue'
-import TabIssueCatalog from '@/modules/tool/components/tabs/IssueCatalog.vue'
-import ToolTabTree from '@/modules/tool/components/tabs/Tree.vue'
+import { ref, watch } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
 
-const props = defineProps({
-  parentId: {
-    type: Object,
-    default: () => ({ id: null, label: null }),
-  },
-})
-
-const tab = ref('Каталог')
+const router = useRouter()
+const route = useRoute()
+const tab = ref('')
 
 const tabs = [
-  { name: 'Каталог', component: TabCatalog, type: 'Catalog' },
-  { name: 'Дерево', component: ToolTabTree },
-  { name: 'Параметры', component: ToolTabParam },
-  { name: 'Выдача', component: TabIssueCatalog, type: 'Get' },
-  { name: 'Склад', component: TabCatalog, type: 'Sklad' },
-  { name: 'История', component: GiveTool },
+  { name: 'Каталог', routeName: 'Catalog' },
+  { name: 'Дерево', routeName: 'Tree' },
+  { name: 'Параметры', routeName: 'ToolTabParam' },
+  { name: 'Выдача', routeName: 'TabIssueCatalog' },
+  { name: 'Склад', routeName: 'TabCatalog' },
+  { name: 'История', routeName: 'GiveTool' },
 ]
+
+watch(
+  () => route.name,
+  (newRouteName) => {
+    tab.value =
+      tabs.find((tab) => tab.routeName === newRouteName)?.name || tabs[0].name
+  }
+)
+
+const navigateToTab = (routeName) => {
+  router.push({ name: routeName })
+}
 </script>
