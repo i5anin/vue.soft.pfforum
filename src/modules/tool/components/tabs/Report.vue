72d0ce478f9a31b0437724858572e79cffde2070
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-01-18 09:39:27 +0300

    Update Report.vue

--- src/modules/tool/components/tabs/Report.vue
+++ src/modules/tool/components/tabs/Report.vue
@@ -5,9 +5,9 @@
         <v-table>
           <thead>
             <tr>
-              <th class="text-left">Отчет</th>
-              <th class="text-left">Действие</th>
-              <th class="text-left">Действие</th>
+              <th class="text-left">Название</th>
+              <th class="text-left">Сформировать</th>
+              <th class="text-left">Просмотр</th>
             </tr>
           </thead>
           <tbody>
@@ -32,13 +32,25 @@
 </template>
 
 <script>
+import { startOfWeek, endOfWeek, format } from 'date-fns'
+
 export default {
   data() {
+    const dateNow = new Date()
+    const dateNextWeek = {
+      start: format(startOfWeek(dateNow), 'yyyy-MM-dd'),
+      end: format(endOfWeek(dateNow), 'yyyy-MM-dd'),
+    }
+    const dateAll = {
+      start: '2020-01-01', // replace with your start date
+      end: format(dateNow, 'yyyy-MM-dd'),
+    }
+
     return {
       reports: [
-        { name: 'Отчет за неделю' },
-        { name: 'Отчет за месяц' },
-        { name: 'Отчет за всё время' },
+        { name: 'Отчет за неделю', date: dateNow },
+        { name: 'Отчет за месяц', date: dateNextWeek },
+        { name: 'Отчет за всё время', date: dateAll },
       ],
     }
   },
