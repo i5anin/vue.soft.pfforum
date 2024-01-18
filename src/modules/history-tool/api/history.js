2fc92cb1f900c3ffaaa6f8b09517e885d4722d27
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-01-18 16:08:57 +0300

    +

--- src/modules/history-tool/api/history.js
+++ src/modules/history-tool/api/history.js
@@ -1,23 +1,10 @@
 import axios from 'axios'
+import { handleApiError } from '@/api/errorHandler'
 
 const axiosInstance = axios.create({ baseURL: import.meta.env.VITE_BASE_URL })
 
 function handleResponse(response) {
   return response.data
-}
-
-function handleApiError(error) {
-  if (error.response) {
-    console.error('Ошибка от сервера:', error.response.data)
-    console.error('Статус:', error.response.status)
-    console.error('Заголовки:', error.response.headers)
-  } else if (error.request) {
-    console.error('Запрос был сделан, но ответ не получен:', error.request)
-  } else {
-    console.error('Ошибка при настройке запроса:', error.message)
-  }
-  console.error('Конфигурация запроса:', error.config)
-  throw error // Перебрасываем ошибку дальше, если нужно дополнительное взаимодействие с ней
 }
 
 export const detailHistoryApi = {
--- src/modules/issue-tool/api/detail.js
+++ src/modules/issue-tool/api/detail.js
@@ -6,20 +6,6 @@ function handleResponse(response) {
   return response.data
 }
 
-function handleApiError(error) {
-  if (error.response) {
-    console.error('Ошибка от сервера:', error.response.data)
-    console.error('Статус:', error.response.status)
-    console.error('Заголовки:', error.response.headers)
-  } else if (error.request) {
-    console.error('Запрос был сделан, но ответ не получен:', error.request)
-  } else {
-    console.error('Ошибка при настройке запроса:', error.message)
-  }
-  console.error('Конфигурация запроса:', error.config)
-  throw error // Перебрасываем ошибку дальше, если нужно дополнительное взаимодействие с ней
-}
-
 export const detailApi = {
   //получить fio
   getDetailFio: async () =>
