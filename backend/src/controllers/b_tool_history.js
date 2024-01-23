6dc8c5a4afd9d869ee6e5de27db55862c46ed5d3
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-01-23 10:18:52 +0300

    Update b_tool_history.js

--- backend/src/controllers/b_tool_history.js
+++ backend/src/controllers/b_tool_history.js
@@ -12,64 +12,6 @@ const dbConfig =
 // Создание соединения с базой данных
 const pool = new Pool(dbConfig)
 
-async function findDetailProduction(req, res) {
-  try {
-    const query = `
-      SELECT
-        CAST(dbo.specs_nom_operations.id AS INTEGER) AS specs_op_id,
-        dbo.specs_nom.ID,
-        dbo.specs_nom.NAME,
-        dbo.specs_nom.description,
-        operations_ordersnom.no,
-        dbo.get_full_cnc_type(dbo.get_op_type_code ( specs_nom_operations.ID )) as cnc_type
-      FROM
-        dbo.specs_nom
-        INNER JOIN dbo.specs_nom_operations ON specs_nom_operations.specs_nom_id = specs_nom.id
-        INNER JOIN dbo.operations_ordersnom ON operations_ordersnom.op_id = specs_nom_operations.ordersnom_op_id
-      WHERE
-        CAST(dbo.specs_nom.ID AS TEXT) LIKE $1
-        AND specs_nom.status_p = 'П'
-        AND NOT specs_nom.status_otgruzka
-        AND ( POSITION('ЗАПРЕТ' IN UPPER(specs_nom.comments)) = 0 OR specs_nom.comments IS NULL )
-        AND (T OR dmc OR hision OR f OR f4 OR fg OR tf)
-      ORDER BY
-        dbo.specs_nom.NAME,
-        dbo.specs_nom.description,
-        operations_ordersnom.no::INT
-    `
-    const result = await pool.query(query, [req.query.id + '%'])
-    res.json(result.rows)
-  } catch (error) {
-    console.error('Ошибка при выполнении запроса:', error)
-    res.status(500).send('Внутренняя ошибка сервера')
-  }
-}
-
-async function getFioOperators(req, res) {
-  try {
-    // SQL запрос для получения всех ID и фамилий из таблицы operators
-    const query = `
-      SELECT id, fio
-      FROM dbo.operators
-      WHERE not nach AND not nalad AND active
-      ORDER BY fio
-    `
-
-    // Выполнение запроса
-    const result = await pool.query(query)
-
-    // Проверка наличия результатов и их отправка
-    if (result.rows && result.rows.length > 0) {
-      res.json(result.rows)
-    } else {
-      res.status(404).send('Операторы не найдены')
-    }
-  } catch (error) {
-    console.error('Ошибка при выполнении запроса:', error)
-    res.status(500).send('Внутренняя ошибка сервера')
-  }
-}
-
 async function getToolHistory(req, res) {
   try {
     const page = parseInt(req.query.page || 1, 10)
@@ -358,62 +300,6 @@ async function saveToolHistory(req, res) {
       },
       requestData: { specs_op_id, id_user, id_tool, quantity, date },
     })
-  }
-}
-
-async function getToolTest(req, res) {
-  try {
-    // Параметры пагинации
-    const page = parseInt(req.query.page || 1, 10)
-    const limit = parseInt(req.query.limit || 15, 10)
-    const offset = (page - 1) * limit
-
-    // Запрос для подсчета общего количества записей
-    const countQuery = `
-      SELECT COUNT(*)
-      FROM dbo.tool_history_nom thn
-             INNER JOIN dbo.specs_nom_operations sno ON thn.specs_op_id = sno.id
-             INNER JOIN dbo.specs_nom sn ON sno.specs_nom_id = sn.id;
-    `
-
-    // Запрос для получения истории инструментов с учетом пагинации
-    const dataQuery = `
-      SELECT thn.specs_op_id                                     AS specs_op_id,
-             sn.ID                                               AS id_part,
-             sn.NAME,
-             sn.description,
-             oon.no                                              AS no_oper,
-             dbo.get_full_cnc_type(dbo.get_op_type_code(sno.ID)) AS type_oper,
-             thn.quantity,
-             tn.name                                             AS name_tool,
-             thn.id_tool,
-             thn.date
-      FROM dbo.tool_history_nom thn
-             INNER JOIN dbo.specs_nom_operations sno ON thn.specs_op_id = sno.id
-             INNER JOIN dbo.specs_nom sn ON sno.specs_nom_id = sn.id
-             INNER JOIN dbo.operations_ordersnom oon ON oon.op_id = sno.ordersnom_op_id
-             INNER JOIN dbo.operators o ON thn.id_user = o.id
-             INNER JOIN dbo.tool_nom tn ON thn.id_tool = tn.id
-      ORDER BY thn.date DESC, sn.NAME, sn.description, oon.no::INT
-      LIMIT ${limit}
-      OFFSET ${offset};
-    `
-    // tn.property
-
-    // Выполнение запросов
-    const countResult = await pool.query(countQuery)
-    const dataResult = await pool.query(dataQuery)
-
-    // Отправка результата
-    res.json({
-      currentPage: page,
-      itemsPerPage: limit,
-      totalCount: parseInt(countResult.rows[0].count, 10),
-      toolsHistory: dataResult.rows,
-    })
-  } catch (err) {
-    console.error('Error executing query', err.stack)
-    res.status(500).send('Ошибка при выполнении запроса')
   }
 }
 
