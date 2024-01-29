86a9ab4f0d0fc0a129a56a8ce8a22367f517e254
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-01-29 09:03:00 +0300

    +
    +

--- src/modules/tool/api/report.js
+++ src/modules/tool/api/report.js
@@ -1,35 +1,37 @@
 import axiosInstance from '@/api/axiosConfig'
 import { handleApiError } from '@/api/errorHandler'
 
+async function downloadReport(endpoint) {
+  try {
+    const response = await axiosInstance.get(endpoint, { responseType: 'blob' })
+
+    // Создаем URL из полученного blob для скачивания
+    const url = window.URL.createObjectURL(new Blob([response.data]))
+    const link = document.createElement('a')
+    link.href = url
+
+    // Получаем текущую дату и время
+    const now = new Date()
+    const dateStr = now.toISOString().replace(/:/g, '.')
+
+    // Устанавливаем имя файла для скачивания
+    link.setAttribute('download', `Report_${dateStr}.xlsx`)
+
+    document.body.appendChild(link)
+    link.click()
+    document.body.removeChild(link)
+    window.URL.revokeObjectURL(url) // Освобождаем ресурсы
+
+    return response
+  } catch (error) {
+    handleApiError(error)
+    throw error
+  }
+}
+
 export const reportApi = {
-  genZayavInstr: async () => {
-    try {
-      // Отправляем запрос, ожидая ответ в формате 'blob'
-      const response = await axiosInstance.get('/report/genZayavInstr', {
-        responseType: 'blob',
-      })
-
-      // Создаем URL из полученного blob для скачивания
-      const url = window.URL.createObjectURL(new Blob([response.data]))
-      const link = document.createElement('a')
-      link.href = url
-
-      // Получаем текущую дату и время
-      const now = new Date()
-      const dateStr = now.toISOString().replace(/:/g, '.')
-
-      // Устанавливаем имя файла для скачивания
-      link.setAttribute('download', `Zakaz_${dateStr}.xlsx`)
-
-      document.body.appendChild(link)
-      link.click()
-      document.body.removeChild(link)
-      window.URL.revokeObjectURL(url) // Освобождаем ресурсы
-
-      return response // Возвращаем ответ для дальнейшей обработки, если это необходимо
-    } catch (error) {
-      handleApiError(error) // Обработка ошибок
-      throw error // Перебрасываем ошибку для дальнейшего уведомления пользователя
-    }
-  },
+  genBuchWeek: () => downloadReport('/report/genBuchWeek'),
+  genBuchEndOp: () => downloadReport('/report/genBuchEndOp'),
+  genBuchMonth: () => downloadReport('/report/genBuchMonth'),
+  genZayavInstr: () => downloadReport('/report/genZayavInstr'),
 }
