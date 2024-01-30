4e68da80fda7cc08524d1a87e70a536bcce78dab
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-01-30 15:29:38 +0300

    Update report.js

--- src/modules/tool/api/report.js
+++ src/modules/tool/api/report.js
@@ -1,36 +1,29 @@
-import axiosInstance from '@/api/axiosConfig'
 import { handleApiError } from '@/api/errorHandler'
+import axiosInstance from '@/api/axiosConfig'
 
-async function downloadReport(endpoint) {
-  try {
-    const response = await axiosInstance.get(endpoint, { responseType: 'blob' })
-
-    // Создаем URL из полученного blob для скачивания
-    const url = window.URL.createObjectURL(new Blob([response.data]))
-    const link = document.createElement('a')
-    link.href = url
-
-    // Получаем текущую дату и время
-    const now = new Date()
-    const dateStr = now.toISOString().replace(/:/g, '.')
-
-    // Устанавливаем имя файла для скачивания
-    link.setAttribute('download', `Report_${dateStr}.xlsx`)
-
-    document.body.appendChild(link)
-    link.click()
-    document.body.removeChild(link)
-    window.URL.revokeObjectURL(url) // Освобождаем ресурсы
-    return response
-  } catch (error) {
-    handleApiError(error)
-    throw error
-  }
+function handleResponse(response) {
+  return response.data
 }
 
 export const reportApi = {
-  genBuchWeek: () => downloadReport('/report/genBuchWeek'),
-  genBuchEndOp: () => downloadReport('/report/genBuchEndOp'),
-  genBuchMonth: () => downloadReport('/report/genBuchMonth'),
-  genZayavInstr: () => downloadReport('/report/genZayavInstr'),
+  genBuchWeek: async () =>
+    axiosInstance
+      .get('/report/genBuchWeek')
+      .then(handleResponse)
+      .catch(handleApiError),
+  genBuchEndOp: async () =>
+    axiosInstance
+      .get('/report/genBuchEndOp')
+      .then(handleResponse)
+      .catch(handleApiError),
+  genBuchMonth: async () =>
+    axiosInstance
+      .get('/report/genBuchMonth')
+      .then(handleResponse)
+      .catch(handleApiError),
+  genZayavInstr: async () =>
+    axiosInstance
+      .get('/report/genZayavInstr')
+      .then(handleResponse)
+      .catch(handleApiError),
 }
