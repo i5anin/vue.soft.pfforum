529d0e06f8e348b6aba643763a2c3aa8271e6eca
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-02-01 16:35:37 +0300

    Update b_buch_end_op.js

--- backend/src/controllers/tool/reports/b_buch_end_op.js
+++ backend/src/controllers/tool/reports/b_buch_end_op.js
@@ -33,35 +33,25 @@ const sentNotifications = []
 
 async function checkStatusChanges() {
   try {
-    console.log('Проверка наличия обновленных строк...')
+    // console.log('Проверка наличия обновленных строк...')
 
-    // Обновляем status_temp в tool_history_nom
-    await pool.query(`
-      UPDATE dbo.tool_history_nom
-      SET status_temp =
-        CASE
-          WHEN (SELECT status_ready FROM dbo.specs_nom_operations WHERE id = tool_history_nom.specs_op_id) = TRUE THEN true
-          WHEN (SELECT status_ready FROM dbo.specs_nom_operations WHERE id = tool_history_nom.specs_op_id) = FALSE THEN false
-          ELSE NULL
-        END;
-    `)
-
-    console.log('Получение обновленных строк для отправки электронной почты...')
+    // console.log('Получение обновленных строк для отправки электронной почты...')
     // Получаем строки для отправки электронной почты
     const { rows } = await pool.query(`
-      SELECT
-        tool_nom.id AS tool_id,
-        tool_nom.name,
+        SELECT
+        tool_nom.ID AS tool_id,
+        tool_nom.NAME,
         tool_history_nom.quantity,
         tool_history_nom.specs_op_id,
-        tool_history_nom.status_temp,
-        (SELECT status_ready FROM dbo.specs_nom_operations WHERE id = tool_history_nom.specs_op_id) AS status_ready
-      FROM
+        tool_history_nom.sent,
+        specs_nom_operations.status_ready
+        FROM
         dbo.tool_history_nom
-      JOIN
-        dbo.tool_nom ON tool_history_nom.id_tool = tool_nom.id
-      WHERE
-        tool_history_nom.status_temp = true;
+        JOIN dbo.tool_nom ON tool_history_nom.id_tool = tool_nom.
+        ID JOIN dbo.specs_nom_operations ON tool_history_nom.specs_op_id = specs_nom_operations.ID
+        WHERE
+        NOT tool_history_nom.sent
+        AND specs_nom_operations.status_ready
     `)
 
     if (rows.length === 0) {
@@ -77,16 +67,17 @@ async function checkStatusChanges() {
 
       // Проверяем, было ли уже отправлено уведомление для данного инструмента
       if (!sentNotifications.includes(row.tool_id)) {
-        // Обновляем данные на актуальные (status_temp = true)
         await pool.query(
           `
-      UPDATE dbo.tool_history_nom
-      SET status_temp = (op.status_ready = true)
-      FROM dbo.specs_nom_operations op
-      WHERE tool_history_nom.specs_op_id = op.id
-        AND tool_history_nom.id_tool = $1;
-      `,
-          [row.tool_id]
+          UPDATE dbo.tool_history_nom
+          SET sent = TRUE
+          FROM
+          dbo.specs_nom_operations
+          WHERE
+          NOT tool_history_nom.sent
+          AND specs_nom_operations.status_ready
+          AND tool_history_nom.specs_op_id = specs_nom_operations.ID
+      `
         )
 
         // Добавляем лог измененного ID истории инструмента
@@ -124,7 +115,6 @@ function sendEmailNotification(row) {
     html: htmlContent,
   }
 
-  return
   transporter.sendMail(mailOptions, function (error, info) {
     if (error) {
       console.log(error)
@@ -140,7 +130,7 @@ function sendEmailNotification(row) {
 }
 
 // Schedule the cron job
-// cron.schedule('*/10 * * * * *', () => {
-//   console.log('Запуск задачи каждые 10 секунд')
-//   checkStatusChanges()
-// })
+cron.schedule('*/10 * * * * *', () => {
+  console.log('Запуск задачи каждые 10 секунд')
+  checkStatusChanges()
+})
