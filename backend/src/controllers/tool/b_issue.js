78d441c9df14136d3cafe61a056d23626f54cd45
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-02-28 13:29:01 +0300

    Update b_issue.js

--- backend/src/controllers/tool/b_issue.js
+++ backend/src/controllers/tool/b_issue.js
@@ -15,27 +15,23 @@ const pool = new Pool(dbConfig)
 async function findDetailProduction(req, res) {
   try {
     const query = `
-      SELECT
-        CAST(dbo.specs_nom_operations.id AS INTEGER) AS specs_op_id,
-        dbo.specs_nom.ID,
-        dbo.specs_nom.NAME,
-        dbo.specs_nom.description,
-        operations_ordersnom.no,
-        dbo.get_full_cnc_type(dbo.get_op_type_code ( specs_nom_operations.ID )) as cnc_type
-      FROM
-        dbo.specs_nom
-        INNER JOIN dbo.specs_nom_operations ON specs_nom_operations.specs_nom_id = specs_nom.id
-        INNER JOIN dbo.operations_ordersnom ON operations_ordersnom.op_id = specs_nom_operations.ordersnom_op_id
-      WHERE
-        CAST(dbo.specs_nom.ID AS TEXT) LIKE $1
+      SELECT CAST(dbo.specs_nom_operations.id AS INTEGER)                         AS specs_op_id,
+             dbo.specs_nom.ID,
+             dbo.specs_nom.NAME,
+             dbo.specs_nom.description,
+             operations_ordersnom.no,
+             dbo.get_full_cnc_type(dbo.get_op_type_code(specs_nom_operations.ID)) as cnc_type
+      FROM dbo.specs_nom
+             INNER JOIN dbo.specs_nom_operations ON specs_nom_operations.specs_nom_id = specs_nom.id
+             INNER JOIN dbo.operations_ordersnom ON operations_ordersnom.op_id = specs_nom_operations.ordersnom_op_id
+      WHERE CAST(dbo.specs_nom.ID AS TEXT) LIKE $1
         AND specs_nom.status_p = 'П'
         AND NOT specs_nom.status_otgruzka
-        AND ( POSITION('ЗАПРЕТ' IN UPPER(specs_nom.comments)) = 0 OR specs_nom.comments IS NULL )
+        AND (POSITION('ЗАПРЕТ' IN UPPER(specs_nom.comments)) = 0 OR specs_nom.comments IS NULL)
         AND (T OR dmc OR hision OR f OR f4 OR fg OR tf)
-      ORDER BY
-        dbo.specs_nom.NAME,
-        dbo.specs_nom.description,
-        operations_ordersnom.no::INT
+      ORDER BY dbo.specs_nom.NAME,
+               dbo.specs_nom.description,
+               operations_ordersnom.no::INT
     `
     const result = await pool.query(query, [req.query.id + '%'])
     res.json(result.rows)
