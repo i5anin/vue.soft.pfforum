d122d40e7e3a2c01b6f75e08cf04ffce312d8eb6
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-04-03 16:55:36 +0300

    Update b_order_Instr.js

--- backend/src/controllers/tool/reports/email/b_order_Instr.js
+++ backend/src/controllers/tool/reports/email/b_order_Instr.js
@@ -207,6 +207,41 @@ async function genZayavInstr(req, res) {
   }
 }
 
+async function getTableReportData(req, res) {
+  try {
+    const query = `
+      SELECT
+          tool_nom.id AS id_tool,
+          tool_nom.name,
+          tool_nom.sklad,
+          tool_nom.norma,
+          tool_nom.norma - tool_nom.sklad AS zakaz,
+          COALESCE(SUM(tool_history_damaged.quantity), 0) AS damaged_last_7_days
+      FROM
+          dbo.tool_nom
+      LEFT JOIN
+          dbo.tool_history_damaged ON tool_nom.id = tool_history_damaged.id_tool
+          AND tool_history_damaged.timestamp >= CURRENT_DATE - INTERVAL '7 days'
+      WHERE
+          tool_nom.norma IS NOT NULL
+          AND (tool_nom.norma - tool_nom.sklad) > 0
+      GROUP BY
+          tool_nom.id,
+          tool_nom.name,
+          tool_nom.sklad,
+          tool_nom.norma
+      ORDER BY
+          tool_nom.id;
+    `
+    const { rows } = await pool.query(query)
+    res.json(rows) // Отправляем данные в формате JSON
+  } catch (error) {
+    console.error('Ошибка при получении данных для таблицы:', error)
+    res.status(500).send('Ошибка при получении данных для таблицы')
+  }
+}
+
 module.exports = {
   genZayavInstr,
+  getTableReportData,
 }
