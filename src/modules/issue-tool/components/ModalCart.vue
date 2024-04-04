56ffd88561802022ce61822672263de7d0764725
Isanin Sergey <sergey.isanin@gmail.com>
2024-04-04 21:56:57 +0300

    Update ModalCart.vue

--- src/modules/issue-tool/components/ModalCart.vue
+++ src/modules/issue-tool/components/ModalCart.vue
@@ -12,6 +12,7 @@
       <v-container>
         <div class="text-h6 pl-5 mb-2">Выбрать деталь:</div>
         <v-row>
+          {{ toolModel }}
           <v-col>
             <v-text-field
               density="compact"
@@ -28,6 +29,7 @@
               :items="options.idNameDescription"
               @update:model-value="onIdSelected"
             />
+
             <v-select
               density="compact"
               label="Номер Тип"
@@ -44,8 +46,6 @@
               item-title="text"
               item-value="value"
               label="ФИО"
-              return-object="false"
-              single-line="false"
               @update:modelValue="handleSelectionChange"
             />
             <v-combobox
@@ -54,8 +54,6 @@
               item-text="title"
               item-value="id"
               label="Тип выдачи"
-              return-object="false"
-              single-line="false"
               :rules="issueTypeRules"
               required
             />
@@ -171,10 +169,8 @@ export default {
     originalData: [],
     idMapping: {},
     fioOptions: [],
-    selectedData: { name: null, description: null, no: null, type: null },
     localParentId: null,
     toolModel: {
-      name: null,
       selectedOperationId: null,
       detailDescription: null,
     },
@@ -248,14 +244,9 @@ export default {
       'updateCartItemQuantityAction',
       'removeFromCartAction',
     ]),
-    onOperationSelected(selectedValue) {
-      const operationId = this.operationMapping[selectedValue]
-      if (operationId) {
-        this.toolModel.operationType = operationId // Установка ID операции, выбранной пользователем
-      } else {
-        console.error('Не удалось найти ID операции для:', selectedValue)
-        this.toolModel.operationType = null
-      }
+    onOperationSelected(value) {
+      this.toolModel.selectedOperationId = this.operationMapping[value]
+      // console.log('Выбран specs_op_id:', id)
     },
     formatToolOptions(data) {
       const uniqueSet = new Set()
@@ -288,16 +279,11 @@ export default {
       if (id) {
         const filteredData = this.originalData.filter((item) => item.id === id)
         this.options.numberType = this.formatOperationOptions(filteredData)
-        // Сбросить выбранное значение для "Номер Тип" каждый раз, когда выбирается новое "Название Обозначение"
-        this.toolModel.operationType = null // Это предполагает, что operationType - это свойство в toolModel, где хранится выбранный "Номер Тип"
       } else {
         console.error(
           'Не удалось найти ID для выбранного значения:',
           selectedValue
         )
-        // Возможно также стоит сбросить options.numberType и toolModel.operationType здесь, если selectedValue не допустимо
-        this.options.numberType = []
-        this.toolModel.operationType = null
       }
     },
 
@@ -310,10 +296,10 @@ export default {
         console.error('Ошибка при поиске:', error)
       }
     },
-
+    // Составление данных для отправки API
     async sendIssueDataToApi() {
+      console.log('sendIssueDataToApi')
       const issueData = {
-        // Составление данных для отправки API
         operationId: this.toolModel.operationType, // Исправлено для использования выбранного типа операции
         userId: this.selectedFio.value, // Исправлено для правильной отправки ID пользователя
         typeIssue: this.toolModel.typeIssue.id,
@@ -416,18 +402,6 @@ export default {
         }
       })
       return Array.from(uniqueSet)
-    },
-
-    initializeLocalState() {
-      if (this.toolId) {
-        this.fetchToolById(this.toolId).then(() => {
-          this.toolModel.sklad = this.tool.sklad
-          this.toolModel.norma = this.tool.norma
-        })
-      } else {
-        this.localParentId = this.idParent.id
-        this.currentFolderName = this.idParent.label
-      }
     },
 
     prependOptionIfNeeded(value, optionsList) {
