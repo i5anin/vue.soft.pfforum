4baf9f18cdb258a80069db083e0e7c26bb61cbf7
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-05-02 14:28:17 +0300

    Revert "Update b_buch_end_op.js"
    This reverts commit 02b18b09c3b477f82127dc567b9aa59b4b612285.

--- backend/src/controllers/tool/reports/email/b_buch_end_op.js
+++ backend/src/controllers/tool/reports/email/b_buch_end_op.js
@@ -48,78 +48,107 @@ const sentNotifications = []
 
 async function checkStatusChanges() {
   try {
-    const operations = await pool.query(`
-      SELECT DISTINCT
-        specs_op_id
+    const { rows } = await pool.query(`
+      SELECT
+        tool_nom.ID AS tool_id,
+        tool_nom.NAME AS tool_name,
+        SUM(tool_history_nom.quantity) AS total_quantity,
+        tool_history_nom.specs_op_id,
+        dbo.kolvo_prod_ready(specs_nom_operations.specs_nom_id) AS quantity_prod,
+        specs_nom_operations.specs_nom_id,
+        specs_nom.NAME AS specs_name,
+        specs_nom.description,
+        operations_ordersnom.no,
+        dbo.get_full_cnc_type(dbo.get_op_type_code(specs_nom_operations.ID)) as cnc_type
       FROM
         dbo.tool_history_nom
+        JOIN dbo.tool_nom ON tool_history_nom.id_tool = tool_nom.ID
+        JOIN dbo.specs_nom_operations ON tool_history_nom.specs_op_id = specs_nom_operations.ID
+        JOIN dbo.specs_nom ON specs_nom_operations.specs_nom_id = specs_nom.ID
+        JOIN dbo.operations_ordersnom ON specs_nom_operations.ordersnom_op_id = operations_ordersnom.ID
       WHERE
-        sent != TRUE
-        AND EXISTS (
-          SELECT 1
-          FROM dbo.specs_nom_operations
-          WHERE
-            dbo.specs_nom_operations.ID = dbo.tool_history_nom.specs_op_id
-            AND (specs_nom_operations.status_ready IS NULL OR specs_nom_operations.status_ready)
-        )
+        (tool_history_nom.sent IS NULL OR NOT tool_history_nom.sent)
+        AND (specs_nom_operations.status_ready IS NULL OR specs_nom_operations.status_ready)
+      GROUP BY
+        tool_nom.ID,
+        tool_nom.NAME,
+        tool_history_nom.specs_op_id,
+        specs_nom_operations.specs_nom_id,
+        specs_nom.NAME,
+        specs_nom.description,
+        operations_ordersnom.no,
+        specs_nom_operations.ID
     `)
 
-    for (const operation of operations.rows) {
-      const tools = await pool.query(
-        `
-        SELECT
-          tool_nom.NAME AS tool_name,
-          SUM(tool_history_nom.quantity) AS total_quantity
-        FROM
-          dbo.tool_history_nom
-        JOIN dbo.tool_nom ON tool_history_nom.id_tool = tool_nom.ID
-        WHERE
-          tool_history_nom.specs_op_id = $1
-          AND tool_history_nom.sent != TRUE
-        GROUP BY
-          tool_nom.NAME
-      `,
-        [operation.specs_op_id]
+    if (rows.length === 0) {
+      console.log('Нет обновлений среди завершенных операций.')
+      return
+    }
+
+    for (const row of rows) {
+      const detailedDescription = `${row.specs_name} - ${row.description} - ${row.no} - ${row.cnc_type}`
+      console.log(
+        `Отправка уведомления для операции: ${detailedDescription}...`
       )
+      let htmlContent = `<h2>Операция завершена: ${detailedDescription}</h2>`
+      htmlContent += '<table border="1"><tr>'
 
-      // Если инструменты есть, объединяем их и отправляем уведомление.
-      if (tools.rows.length > 0) {
-        let htmlContent = `<h2>Операция завершена: ID ${operation.specs_op_id}</h2>`
-        htmlContent +=
-          '<table border="1"><tr><th>Название инструмента</th><th>Кол-во выдано</th></tr>'
+      const tableStructure = [
+        { label: 'specs_op_id', text: 'ID операции' },
+        { label: 'specs_nom_id', text: 'ID партии' },
+        { label: 'tool_name', text: 'Название инструмента' },
+        { label: 'total_quantity', text: 'Кол-во выдано' },
+        { label: 'quantity_prod', text: 'Кол-во продукции' },
+        // { label: 'specs_name', text: 'Название' },
+        // { label: 'description', text: 'Описание' },
+        // { label: 'no', text: 'Номер' },
+        // { label: 'cnc_type', text: 'Тип CNC' },
+      ]
 
-        tools.rows.forEach((tool) => {
-          htmlContent += `<tr><td>${tool.tool_name}</td><td>${tool.total_quantity}</td></tr>`
-        })
-        htmlContent += '</table>'
+      // Добавляем заголовки в таблицу
+      tableStructure.forEach((header) => {
+        htmlContent += `<th>${header.text}</th>`
+      })
+      htmlContent += '</tr><tr>'
 
-        const mailOptions = {
-          from: process.env.MAIL_USER,
-          to: process.env.MAIL_TO,
-          subject: `Уведомление о завершении операции ID ${operation.specs_op_id}`,
-          html: htmlContent,
-        }
+      // Добавляем данные для строки
+      tableStructure.forEach((header) => {
+        htmlContent += `<td>${row[header.label] ?? 'N/A'}</td>`
+      })
+      htmlContent += '</tr></table>'
 
-        transporter.sendMail(mailOptions, function (error, info) {
-          if (error) {
-            console.log(error)
-          } else {
-            console.log(
-              `Отправлено уведомление для операции: ID ${operation.specs_op_id}. Status info: ${info.response}`
-            )
-          }
-        })
+      console.log('From:', process.env.MAIL_USER)
+      console.log('To:', process.env.MAIL_TO)
 
-        // После успешной отправки обновляем статус sent для всех инструментов этой операции
-        await pool.query(
-          `
-          UPDATE dbo.tool_history_nom
-          SET sent = TRUE
-          WHERE specs_op_id = $1
-        `,
-          [operation.specs_op_id]
-        )
+      const mailOptions = {
+        from: process.env.MAIL_USER,
+        to: process.env.MAIL_TO,
+        subject: `Бухгалтерия: отчет по завершению операции: ${detailedDescription}`,
+        html: htmlContent,
       }
+
+      if (!mailOptions.from || !mailOptions.to) {
+        throw new Error('Не указан адрес отправителя или получателя')
+      }
+
+      // Отправка уведомления
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          console.log(error)
+        } else {
+          console.log(
+            `Отправлено уведомление для операции: ${detailedDescription}. Status info: ${info.response}`
+          )
+        }
+      })
+
+      // !!! Обновляем статус отправки для обработанных строк
+      await pool.query(
+        `UPDATE dbo.tool_history_nom
+         SET sent = TRUE
+         WHERE specs_op_id = $1 AND NOT sent`,
+        [row.specs_op_id]
+      )
     }
   } catch (error) {
     console.error('Ошибка при проверке статуса изменений:', error)
@@ -128,8 +157,8 @@ async function checkStatusChanges() {
 
 // Schedule the cron job
 min15 = '0 */15 * * * *'
-sec30 = '*/30 * * * * *'
-cron.schedule(sec30, () => {
+sec10 = '*/10 * * * * *'
+cron.schedule(min15, () => {
   checkStatusChanges()
     .then(() => {
       // console.log('Задача выполнена успешно.')
