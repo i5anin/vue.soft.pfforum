569f14697c7931a8e510a4a1a46782556963b329
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-05-06 09:59:44 +0300

    Update b_buch_week.js

--- backend/src/controllers/tool/reports/vue/b_buch_week.js
+++ backend/src/controllers/tool/reports/vue/b_buch_week.js
@@ -15,34 +15,44 @@ const pool = new Pool(dbConfig)
 
 async function getTableReportData(req, res) {
   try {
+    // Определите начало и конец недели, начиная с текущего или последнего прошедшего четверга
+    const today = new Date()
+    const thisThursday = new Date(today)
+    thisThursday.setDate(
+      thisThursday.getDate() - ((thisThursday.getDay() + 3) % 7)
+    ) // Находим прошлый четверг, если сегодня не четверг
+    const nextThursday = new Date(thisThursday)
+    nextThursday.setDate(nextThursday.getDate() + 7) // Следующий четверг
+
+    const startDate = thisThursday.toISOString().split('T')[0]
+    const endDate = nextThursday.toISOString().split('T')[0]
+
     const query = `WITH RECURSIVE
                      TreePath AS (SELECT id,
                                          name,
                                          parent_id,
-                                         name AS path -- Начальный путь это просто имя текущей записи
+                                         name AS path
                                   FROM dbo.tool_tree
-                                  WHERE parent_id = 1 -- Стартуем с корня иерархии
+                                  WHERE parent_id = 1
 
                                   UNION ALL
 
                                   SELECT tt.id,
                                          tt.name,
                                          tt.parent_id,
-                                         CONCAT(tp.path, ' / ', tt.name) -- Строим путь, добавляя имя текущей записи
+                                         CONCAT(tp.path, ' / ', tt.name)
                                   FROM dbo.tool_tree tt
-                                         JOIN TreePath tp ON tt.parent_id = tp.id -- Присоединяем дочерние элементы к родительским
-                     ),
+                                         JOIN TreePath tp ON tt.parent_id = tp.id),
                      damaged AS (SELECT tool_nom.id                     AS id_tool,
                                         tool_nom.parent_id,
                                         tool_nom.name,
                                         tool_nom.sklad,
                                         tool_nom.norma,
                                         tool_nom.norma - tool_nom.sklad AS zakaz
-                                 -- COALESCE(SUM(tool_history_damaged.quantity), 0) AS damaged_last_7_days
                                  FROM dbo.tool_nom
-                                        LEFT JOIN
-                                      dbo.tool_history_damaged ON tool_nom.id = tool_history_damaged.id_tool
-                                        AND tool_history_damaged.timestamp >= CURRENT_DATE - INTERVAL '7 days'
+                                        LEFT JOIN dbo.tool_history_damaged ON tool_nom.id = tool_history_damaged.id_tool
+                                   AND tool_history_damaged.timestamp >= '${startDate}'::date
+                                   AND tool_history_damaged.timestamp < '${endDate}'::date
                                  WHERE tool_nom.norma IS NOT NULL
                                    AND (tool_nom.norma - tool_nom.sklad) > 0
                                  GROUP BY tool_nom.id,
@@ -58,7 +68,6 @@ async function getTableReportData(req, res) {
                               'sklad', d.sklad,
                               'norma', d.norma,
                               'zakaz', d.zakaz
-                            --'damaged_last_7_days', d.damaged_last_7_days
                             )
                           ) AS tools
                    FROM damaged d
@@ -67,11 +76,15 @@ async function getTableReportData(req, res) {
                    ORDER BY tp.path;
     `
     const { rows } = await pool.query(query)
-    res.json(rows) // Отправляем данные в формате JSON
+    res.json(rows)
   } catch (error) {
     console.error('Ошибка при получении данных для таблицы:', error)
     res.status(500).send('Ошибка при получении данных для таблицы')
   }
+}
+
+module.exports = {
+  getTableReportData,
 }
 
 module.exports = {
