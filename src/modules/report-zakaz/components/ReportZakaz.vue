1fcd39b8b401442cefbfb290e9f4b75ef0b43f56
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-05-07 08:46:43 +0300

    Update ReportZakaz.vue

--- src/modules/report-zakaz/components/ReportZakaz.vue
+++ src/modules/report-zakaz/components/ReportZakaz.vue
@@ -7,7 +7,13 @@
         </template>
         {{ group.path }}
       </v-chip>
-      <v-chip variant="flat" color="red">{{ group.tools.length }}</v-chip>
+      <v-chip color="while">{{ group.tools.length }}</v-chip>
+      <v-icon
+        v-if="checkTools(group)"
+        color="red"
+        icon=" mdi-alert"
+        title="Есть позиции мение 20%"
+      />
       <div v-if="visibleGroups.includes(index)">
         <v-table dense>
           <thead>
@@ -17,17 +23,34 @@
               <th class="text-left">Заказ</th>
               <th class="text-left">Склад</th>
               <th class="text-left">Норма</th>
-              <th class="text-left">Повреждено за 7 дней</th>
+              <th class="text-left">Процент не хватает</th>
+              <!-- <th class="text-left">Повреждено за 7 дней</th>-->
             </tr>
           </thead>
           <tbody>
             <tr v-for="(tool, toolIndex) in group.tools" :key="toolIndex">
               <td class="grey">{{ toolIndex + 1 }}</td>
-              <td>{{ tool.name }}</td>
+              <td
+                :style="{
+                  'text-decoration':
+                    ((1 - tool.sklad / tool.norma) * 100).toFixed(2) < 20
+                      ? 'underline'
+                      : 'none',
+                  'text-decoration-color':
+                    ((1 - tool.sklad / tool.norma) * 100).toFixed(2) < 20
+                      ? 'red'
+                      : '',
+                }"
+              >
+                {{ tool.name }}
+              </td>
               <td>{{ tool.zakaz }}</td>
               <td class="grey">{{ tool.sklad }}</td>
               <td class="grey">{{ tool.norma }}</td>
-              <td class="grey">{{ tool.damaged_last_7_days }}</td>
+              <td class="grey">
+                {{ ((1 - tool.sklad / tool.norma) * 100).toFixed(2) }} %
+              </td>
+              <!--              <td class="grey">{{ tool.damaged_last_7_days }}</td>-->
             </tr>
           </tbody>
         </v-table>
@@ -50,6 +73,17 @@ export default {
     this.fetchZakazData()
   },
   methods: {
+    checkTools(group) {
+      let totalSklad = 0
+      let totalNorma = 0
+
+      group.tools.forEach((tool) => {
+        totalSklad += tool.sklad
+        totalNorma += tool.norma
+      })
+      console.log(((1 - totalSklad / totalNorma) * 100).toFixed(2))
+      return ((1 - totalSklad / totalNorma) * 100).toFixed(2) <= 20
+    },
     async fetchZakazData() {
       try {
         this.toolGroups = await reportApi.getZakaz() // Убедитесь, что это правильный путь к данным в вашем API
@@ -77,5 +111,9 @@ export default {
 
 .tool-group + .tool-group {
   margin-top: 10px;
+}
+
+.red-text {
+  color: red;
 }
 </style>
