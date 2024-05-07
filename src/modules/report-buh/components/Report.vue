6cc5cb6c97755ee413958d22d7071dd4aadab539
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-05-07 22:14:16 +0300

    Update Report.vue

--- src/modules/report-buh/components/Report.vue
+++ src/modules/report-buh/components/Report.vue
@@ -6,16 +6,13 @@
           <thead>
             <tr>
               <th class="text-left">Название</th>
-              <th class="text-left">Расчетный период</th>
               <th class="text-left">Ближайшая дата</th>
               <th class="text-left">На почту</th>
-              <!-- <th class="text-left">Загрузить</th>-->
             </tr>
           </thead>
           <tbody>
             <tr v-for="(report, index) in reports" :key="index">
               <td>{{ report.name }}</td>
-              <td>{{ report.period }}</td>
               <td>{{ report.info }}</td>
               <td>
                 <v-btn color="primary" @click="report.action(report)">
@@ -32,7 +29,6 @@
 </template>
 <script>
 import ReportZakaz from './ReportBuh.vue'
-import { reportApi } from '@/modules/report-buh/api/report'
 
 export default {
   components: { ReportZakaz },
@@ -42,96 +38,17 @@ export default {
         {
           name: 'Исключен сломанный',
           info: 'раз в неделю каждый ПТ в 12:00 (за неделю)',
-          action: this.genBuchIssuedWeek,
-          period: '',
         },
         {
           name: 'По завершению операции',
-          info: '-',
-          action: this.genBuchEndOp,
-          period: '',
+          info: 'отправлено на почту',
         },
         {
           name: 'Журнал поврежденного',
           info: 'раз в месяц каждый ПТ в 12:00 (за месяц)',
-          action: this.genBuchMonth,
-          period: '',
         },
-        // {
-        //   name: 'Отчет заявка на инструмент',
-        //   info: 'раз в неделю каждый ЧТ в 12:00 (за неделю)',
-        //   action: this.genZayavInstrWeek,
-        // },
       ],
     }
-  },
-  mounted() {
-    this.genBuchIssuedWeek()
-    this.genBuchMonth()
-  },
-
-  methods: {
-    async genBuchEndOp() {
-      await reportApi.genBuchEndOp()
-    },
-
-    async genZayavInstrWeek() {
-      await reportApi.genZayavInstr()
-    },
-
-    genBuchIssuedWeek() {
-      const now = new Date()
-      const lastThursday = new Date()
-      lastThursday.setDate(now.getDate() - ((now.getDay() + 3) % 7))
-      const nextThursday = new Date(lastThursday)
-      nextThursday.setDate(nextThursday.getDate() + 7)
-
-      const string_lastThursday = `${lastThursday
-        .getDate()
-        .toString()
-        .padStart(2, '0')} ${lastThursday
-        .getMonth()
-        .toString()
-        .padStart(2, '0')} ${lastThursday.getFullYear()} 00 00`
-      const string_nextThursday = `${nextThursday
-        .getDate()
-        .toString()
-        .padStart(2, '0')} ${nextThursday
-        .getMonth()
-        .toString()
-        .padStart(2, '0')} ${nextThursday.getFullYear()} 00 00`
-      this.reports.find(
-        (report) => report.action === this.genBuchIssuedWeek
-      ).period = `${string_lastThursday} - ${string_nextThursday}`
-    },
-
-    genBuchMonth() {
-      const now = new Date()
-      const firstDayOfMonth = new Date(now.getFullYear(), now.getMonth(), 1)
-      const firstDayOfNextMonth = new Date(
-        now.getFullYear(),
-        now.getMonth() + 1,
-        1
-      )
-
-      const string_firstDayOfMonth = `${firstDayOfMonth
-        .getDate()
-        .toString()
-        .padStart(2, '0')} ${firstDayOfMonth
-        .getMonth()
-        .toString()
-        .padStart(2, '0')} ${firstDayOfMonth.getFullYear()} 00 00`
-      const string_firstDayOfNextMonth = `${firstDayOfNextMonth
-        .getDate()
-        .toString()
-        .padStart(2, '0')} ${firstDayOfNextMonth
-        .getMonth()
-        .toString()
-        .padStart(2, '0')} ${firstDayOfNextMonth.getFullYear()} 00 00`
-      this.reports.find(
-        (report) => report.action === this.genBuchMonth
-      ).period = `${string_firstDayOfMonth} - ${string_firstDayOfNextMonth}`
-    },
   },
 }
 </script>
