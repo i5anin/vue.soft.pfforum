d680ebd2278c74310f093fc4112689591666c53d
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-05-08 09:18:13 +0300

    Вынос конфигурации базы данных в файл
    Update databaseConfig.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,6 +10,24 @@ const networkDetails = getNetworkDetails()
 const dbConfig = getDbConfig()
 const app = express()
 
+console.log('\x1b[36m%s\x1b[0m', '• Почтовый сервер:')
+console.log('Host:', process.env.MAIL_HOST)
+console.log('Port:', process.env.MAIL_PORT)
+console.log('User:', process.env.MAIL_USER)
+console.log('Pass:', process.env.MAIL_PASS)
+console.log('From:', process.env.MAIL_USER)
+console.log('To:', process.env.MAIL_TO)
+
+console.log('\x1b[31m%s\x1b[0m', '\n• Конфигурация базы данных:')
+console.log('User:', process.env.DB_USER || process.env.DB_TEST_USER)
+console.log('Host:', process.env.DB_HOST || process.env.DB_TEST_HOST)
+console.log('Database:', process.env.DB_NAME || process.env.DB_TEST_NAME)
+console.log(
+  'Password:',
+  process.env.DB_PASSWORD || process.env.DB_TEST_PASSWORD
+)
+console.log('Port:', process.env.DB_PORT || process.env.DB_TEST_PORT)
+
 app.use(bodyParser.json())
 app.use(cors())
 app.use('/api', routers)
