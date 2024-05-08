232e7f2e4adb20c22996bcab0554b71c3782fcc0
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-05-08 09:18:13 +0300

    Update databaseConfig.js

--- backend/src/databaseConfig.js
+++ backend/src/databaseConfig.js
@@ -4,16 +4,16 @@ const express = require('express')
 const app = express()
 
 function getDbConfig() {
-  const networkDetails = 'определить ip клиента!!!'
+  let databaseType = process.env.NODE_ENV
+  console.log(databaseType)
+  // const networkDetails = 'определить ip клиента!!!'
 
-  if (networkDetails.ip === '192.168.0.200') {
-    console.log(config.dbConfigSuperTest)
-    return config.dbConfigSuperTest
-  } else {
-    return networkDetails.databaseType === 'build'
-      ? config.dbConfig
-      : config.dbConfigTest
-  }
+  // if (networkDetails.ip === '192.168.0.200') {
+  //   console.log(config.dbConfigSuperTest)
+  //   return config.dbConfigSuperTest
+  // } else {
+  return databaseType === 'build' ? config.dbConfig : config.dbConfigTest
+  // }
 }
 
 module.exports = getDbConfig
