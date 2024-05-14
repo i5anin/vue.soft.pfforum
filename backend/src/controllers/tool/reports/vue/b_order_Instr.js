6fc1b2fe2f7bf4b3d2eaadb0b8011149570e0ea6
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-05-14 15:38:02 +0300

    Update b_order_Instr.js

--- backend/src/controllers/tool/reports/vue/b_order_Instr.js
+++ backend/src/controllers/tool/reports/vue/b_order_Instr.js
@@ -23,8 +23,7 @@ async function getTableReportData(req, res) {
                                          tt.parent_id,
                                          CONCAT(tp.path, ' / ', tt.name) -- Строим путь, добавляя имя текущей записи
                                   FROM dbo.tool_tree tt
-                                         JOIN TreePath tp ON tt.parent_id = tp.id
-                     ),
+                                         JOIN TreePath tp ON tt.parent_id = tp.id),
                      damaged AS (SELECT tool_nom.id                                     AS id_tool,
                                         tool_nom.parent_id,
                                         tool_nom.group_id,
@@ -36,7 +35,7 @@ async function getTableReportData(req, res) {
                                         COALESCE(SUM(tool_history_damaged.quantity), 0) AS damaged_last_7_days
                                  FROM dbo.tool_nom
                                         LEFT JOIN dbo.tool_history_damaged ON tool_nom.id = tool_history_damaged.id_tool
-                                        AND tool_history_damaged.timestamp >= CURRENT_DATE - INTERVAL '7 days'
+                                   AND tool_history_damaged.timestamp >= CURRENT_DATE - INTERVAL '7 days'
                                  WHERE tool_nom.norma IS NOT NULL
                                    AND (tool_nom.norma - tool_nom.sklad) > 0
                                  GROUP BY tool_nom.id,
@@ -51,13 +50,13 @@ async function getTableReportData(req, res) {
                           JSON_AGG(
                             JSON_BUILD_OBJECT(
                               'id_tool', d.id_tool,
-                              'group_standard', d.group_standard,
                               'name', d.name,
-                              'group_id', d.group_id,
                               'sklad', d.sklad,
                               'norma', d.norma,
                               'zakaz', d.zakaz,
-                              'damaged_last_7_days', d.damaged_last_7_days
+                              'damaged_last_7_days', d.damaged_last_7_days,
+                              'group_id', d.group_id,
+                              'group_standard', d.group_standard
                             )
                           ) AS tools
                    FROM damaged d
