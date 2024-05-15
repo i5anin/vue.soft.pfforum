8a819c8f9891690866aa8d8ae5620e7de148ad4f
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-05-15 09:42:45 +0300

    Отчет заказ
    Update b_order_Instr.js
    
    Update b_order_Instr.js
    
    Update b_order_Instr.js
    
    Update ReportZakazTable.vue
    
    Update b_order_Instr.js
    
    Update b_order_Instr.js
    
    Update b_order_Instr.js
    
    Update b_order_Instr.js
    
    Update b_order_Instr.js
    
    Update b_order_Instr.js
    
    Update b_order_Instr.js
    
    Update b_order_Instr.js
    
    w
    
    Update b_order_Instr.js
    
    Update b_order_Instr.js
    
    Update b_order_Instr.js

--- backend/src/controllers/tool/reports/vue/b_order_Instr.js
+++ backend/src/controllers/tool/reports/vue/b_order_Instr.js
@@ -23,9 +23,10 @@ async function getTableReportData(req, res) {
                                   FROM dbo.tool_tree tt
                                          JOIN TreePath tp ON tt.parent_id = tp.id),
                      totals AS (SELECT group_id,
-                                        SUM(sklad) AS group_total_sklad
-                                 FROM dbo.tool_nom
-                                 GROUP BY group_id),
+                                       SUM(sklad) AS group_total_sklad
+                                FROM dbo.tool_nom
+                                WHERE group_id IS NOT NULL AND group_id <> 0
+                                GROUP BY group_id),
                      damaged AS (SELECT tn.id                                     AS id_tool,
                                         tn.parent_id,
                                         tn.group_id,
@@ -34,20 +35,22 @@ async function getTableReportData(req, res) {
                                         tn.sklad,
                                         tn.norma,
                                         CASE
-                                          WHEN tn.group_standard AND t.group_total_sklad IS NOT NULL THEN GREATEST(tn.norma - t.group_total_sklad, 0)
+                                          WHEN tn.group_id IS NOT NULL AND tn.group_id <> 0
+                                            THEN GREATEST(tn.norma - t.group_total_sklad, 0)
                                           ELSE GREATEST(tn.norma - tn.sklad, 0)
-                                        END                                            AS zakaz,
+                                          END                                            AS zakaz,
                                         COALESCE(SUM(thd.quantity), 0)           AS damaged_last_7_days,
                                         t.group_total_sklad                      AS group_sum
                                  FROM dbo.tool_nom tn
-                                 LEFT JOIN dbo.tool_history_damaged thd ON tn.id = thd.id_tool
-                                 AND thd.timestamp >= CURRENT_DATE - INTERVAL '7 days'
-                                 LEFT JOIN totals t ON tn.group_id = t.group_id
+                                        LEFT JOIN dbo.tool_history_damaged thd ON tn.id = thd.id_tool
+                                   AND thd.timestamp >= CURRENT_DATE - INTERVAL '7 days'
+                                        LEFT JOIN totals t ON tn.group_id = t.group_id
                                  GROUP BY tn.id, tn.parent_id, tn.name, tn.sklad, tn.norma, tn.group_id, tn.group_standard, t.group_total_sklad
                                  HAVING CASE
-                                          WHEN tn.group_standard AND t.group_total_sklad IS NOT NULL THEN GREATEST(tn.norma - t.group_total_sklad, 0)
+                                          WHEN tn.group_id IS NOT NULL AND tn.group_id <> 0
+                                            THEN GREATEST(tn.norma - t.group_total_sklad, 0)
                                           ELSE GREATEST(tn.norma - tn.sklad, 0)
-                                        END > 0)
+                                          END > 0)
                    SELECT d.parent_id,
                           tp.path,
                           JSON_AGG(
