0d35960f8d056958883d00c4cc202ace0ce5caf5
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-06-10 11:13:24 +0300

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,24 +2,25 @@ module.exports = {
   apps: [
     {
       name: 'backend soft',
-      script: 'nodemon',
-      args: 'backend/src/app.js',
-      watch: true,
+      script: 'node',
+      args: 'backend/build/app.js', // предполагается, что ваш бэкенд собран в папку backend/build
+      watch: ['backend/build'],
+      ignore_watch: ['node_modules'],
       env: {
         PM2_PUBLIC_KEY: process.env.PM2_PUBLIC_KEY,
         PM2_SECRET_KEY: process.env.PM2_SECRET_KEY,
         NODE_ENV: process.env.NODE_ENV,
       },
       instances: 1,
-      exec_mode: 'cluster',
+      exec_mode: 'fork', // Используйте режим fork для backend
     },
     {
       name: 'frontend soft',
-      script: 'npx',
-      args: 'vite --host',
+      script: 'serve',
+      args: '-s dist -l 3000', // указываем папку dist и порт 3000
       interpreter: 'none',
       instances: 1,
-      watch: true,
+      exec_mode: 'fork', // Для фронтенда также можно использовать fork
       env: {
         PM2_PUBLIC_KEY: process.env.PM2_PUBLIC_KEY,
         PM2_SECRET_KEY: process.env.PM2_SECRET_KEY,
