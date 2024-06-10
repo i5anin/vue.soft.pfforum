640ddeae5befb2801e857f21a7dea8e8ca6a34cf
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-06-10 15:00:19 +0300

    axiosInstance

--- src/api/get_nom.js
+++ src/api/get_nom.js
@@ -33,7 +33,7 @@ export const toolApi = {
       ...filters,
     }
 
-    return axiosInstanceLaravel
+    return axiosInstance
       .get('/tools', { params })
       .then(handleResponse)
       .catch(handleApiError)
@@ -41,7 +41,7 @@ export const toolApi = {
 
   // Получить инструмент по ID инструмента
   getToolById: async (id) => {
-    return axiosInstanceLaravel
+    return axiosInstance
       .get(`/tool/${id}`)
       .then(handleResponse)
       .catch(handleApiError)
@@ -49,7 +49,7 @@ export const toolApi = {
 
   // Получить фильтры по ID папки
   filterParamsByParentId: async (parent_id) =>
-    axiosInstanceLaravel
+    axiosInstance
       .get(`/filter-params/${parent_id}`)
       .then(handleResponse)
       .catch(handleApiError),
--- src/modules/editor/api/editor.js
+++ src/modules/editor/api/editor.js
@@ -8,28 +8,28 @@ function handleResponse(response) {
 export const editorToolApi = {
   // Добавить новый инструмент
   addTool: async (toolData) =>
-    axiosInstanceLaravel
+    axiosInstance
       .post('/tool', toolData)
       .then(handleResponse)
       .catch(handleApiError),
 
   // Удалить инструмент по ID
   deleteTool: async (id) =>
-    axiosInstanceLaravel
+    axiosInstance
       .delete(`/tool/${id}`)
       .then(handleResponse)
       .catch(handleApiError),
 
   // Обновить существующий инструмент
   updateTool: async (id, toolData) =>
-    axiosInstanceLaravel
+    axiosInstance
       .put(`/tool/${id}`, toolData)
       .then(handleResponse)
       .catch(handleApiError),
 
   // Получить параметры инструмента по parent_id
   getToolParamsByParentId: async (parentId) =>
-    axiosInstanceLaravel
+    axiosInstance
       .get(`/tools-params/${parentId}`)
       .then(handleResponse)
       .catch(handleApiError),
