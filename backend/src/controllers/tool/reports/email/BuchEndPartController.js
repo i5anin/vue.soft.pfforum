db665fd24e20ba9f6d42ee66a9028a66567efe4e
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-06-19 11:44:14 +0300

    Update BuchEndPartController.js

--- backend/src/controllers/tool/reports/email/BuchEndPartController.js
+++ backend/src/controllers/tool/reports/email/BuchEndPartController.js
@@ -4,13 +4,11 @@ const { emailConfig } = require('../../../../config/config')
 const { htmlToText } = require('nodemailer-html-to-text')
 const getEmailRecipients = require('./getEmailRecipients')
 const getDbConfig = require('../../../../config/databaseConfig')
-
-// Настройка подключения к базе данных
+const cron = require('node-cron')
 
 const dbConfig = getDbConfig()
 const pool = new Pool(dbConfig)
 
-// Set up Nodemailer
 const transporter = nodemailer.createTransport({
   host: emailConfig.host,
   port: emailConfig.port,
@@ -21,157 +19,122 @@ const transporter = nodemailer.createTransport({
   },
 })
 
-// Use htmlToText plugin
 transporter.use('compile', htmlToText())
 
-// Функция для формирования содержимого электронного письма
-function createMailContent(tools, specsOpId) {
-  let htmlContent = `<h2>Операция завершена: ${specsOpId}</h2>`
-  if (tools.length > 0) {
-    const firstTool = tools[0]
-    if (!firstTool.specs_name)
-      throw new Error('Отсутствуют данные для формирования уведомления.')
+function createMailContent(tools, partId) {
+  let htmlContent = `<h2>Отчет по инструментам для партии: ${partId}</h2>`
 
-    htmlContent += `<p>${firstTool.specs_name} - ${firstTool.description} - ${
-      firstTool.no || ''
-    } - ${firstTool.cnc_type}</p>`
-    htmlContent += `<h3>Кол-во продукции: ${firstTool.quantity_prod}</h3>`
-    htmlContent += `<table border='1'><tr><th>Название инструмента</th><th>Кол-во выдано</th></tr>`
+  if (tools.length > 0) {
+    htmlContent += `
+      <table border='1'>
+        <tr>
+          <th>ID инструмента</th>
+          <th>Название инструмента</th>
+          <th>Количество</th>
+        </tr>
+    `
     tools.forEach((tool) => {
-      htmlContent += `<tr><td>${tool.tool_name}</td><td>${tool.total_quantity}</td></tr>`
+      htmlContent += `
+        <tr>
+          <td>${tool.id_tool}</td>
+          <td>${tool.tool_name}</td>
+          <td>${tool.quantity}</td>
+        </tr>
+      `
     })
     htmlContent += `</table>`
+  } else {
+    htmlContent += '<p>Инструменты для данной партии не найдены.</p>'
   }
+
   return htmlContent
 }
 
-async function checkStatusChanges() {
-  let financeUserEmail
-  let adminUserEmail
+async function sendReportForPart(partId) {
   try {
-    // Выбираем уникальные ID операций, для которых уведомления еще не отправлены
-    const operationsResult = await pool.query(`
-      SELECT DISTINCT
-        thn.specs_op_id
-      FROM
-        "dbo"."tool_history_nom" AS thn
-          LEFT JOIN
-        "dbo"."specs_nom_operations" AS sno ON thn.specs_op_id = sno.id
-      WHERE
-        sno.status_ready = true AND thn.sent IS NULL;
-    `)
+    const toolsResult = await pool.query(
+      `
+        SELECT thn.id,
+               thn.id_tool,
+               tn.name AS tool_name,
+               thn.quantity
+        FROM dbo.tool_history_nom thn
+               JOIN dbo.tool_nom tn ON thn.id_tool = tn.id
+        WHERE thn.specs_nom_id = $1
+      `,
+      [partId],
+    )
 
-    const operations = operationsResult.rows
+    const tools = toolsResult.rows
 
-    // Если операций нет, то ничего не делаем
-    if (operations.length === 0) {
-      console.log('Нет операций для отправки уведомлений.')
+    if (tools.length === 0) {
+      console.log(`Нет инструментов для отправки отчета по партии: ${partId}`)
       return
     }
 
-    // Перебираем операции и отправляем уведомления
-    for (const operation of operations) {
-      const specsOpId = operation.specs_op_id
+    const htmlContent = createMailContent(tools, partId)
+    const financeUserEmail = await getEmailRecipients('finance')
 
-      // Получаем данные для отправки уведомлений, сгруппированные по операции
-      const toolsResult = await pool.query(
-        `
-          SELECT tool_nom.NAME                                           AS tool_name,
-                 SUM(tool_history_nom.quantity)                          AS total_quantity,
-                 dbo.kolvo_prod_ready(specs_nom_operations.specs_nom_id) AS quantity_prod,
-                 specs_nom_operations.specs_nom_id,
-                 specs_nom.NAME                                          AS specs_name,
-                 specs_nom.description                                   AS description,
-                 operations_ordersnom.no AS no,
-        dbo.get_full_cnc_type(dbo.get_op_type_code(specs_nom_operations.ID)) AS cnc_type
-          FROM dbo.tool_history_nom
-            LEFT JOIN dbo.tool_nom
-          ON tool_history_nom.id_tool = tool_nom.ID
-            LEFT JOIN dbo.specs_nom_operations ON tool_history_nom.specs_op_id = specs_nom_operations.ID
-            LEFT JOIN dbo.specs_nom ON specs_nom_operations.specs_nom_id = specs_nom.ID
-            LEFT JOIN dbo.operations_ordersnom ON specs_nom_operations.ordersnom_op_id = operations_ordersnom.ID
-          WHERE tool_history_nom.specs_op_id = $1
-            AND tool_history_nom.quantity != 0
-          GROUP BY tool_nom.NAME,
-            specs_nom_operations.specs_nom_id,
-            specs_nom.NAME,
-            specs_nom.description,
-            operations_ordersnom.no,
-            specs_nom_operations.ID
-        `,
-        [specsOpId]
-      )
+    let mailOptions = {}
 
-      const tools = toolsResult.rows
-
-      // Если для указанной операции нет инструментов в базе данных
-      if (tools.length === 0) {
-        await console.log(specsOpId, 'Ошибка: набор tools пуст.', pool)
-        continue
+    if (process.env.VITE_NODE_ENV === 'build' && financeUserEmail) {
+      mailOptions = {
+        from: process.env.MAIL_USER,
+        to: financeUserEmail,
+        subject: `Отчет по инструментам для партии: ${partId}`,
+        html: htmlContent,
       }
-
-      // Формируем HTML уведомления
-      const htmlContent = createMailContent(tools, specsOpId)
-
-      financeUserEmail = await getEmailRecipients('finance')
-      adminUserEmail = await getEmailRecipients('admin')
-
-      let mailOptions = {}
-
-      if (process.env.VITE_NODE_ENV === 'build' && financeUserEmail) {
-        mailOptions = {
-          from: process.env.MAIL_USER,
-          to: financeUserEmail,
-          subject: `Отчет по завершению операции: ${specsOpId}`,
-          html: htmlContent,
-        }
-      } else if (adminUserEmail) {
-        mailOptions = {
-          from: process.env.MAIL_USER,
-          to: adminUserEmail,
-          subject: `Отчет по завершению операции для администратора: ${specsOpId}`,
-          html: htmlContent,
-        }
-      }
-
-      // Отправка уведомления
-      transporter.sendMail(mailOptions, async function (error, info) {
-        if (error) {
-          console.log(error)
-        } else {
-          console.log(
-            `Отправлено уведомление для операции: ${specsOpId}. Status info: ${info.response}`
-          )
-
-          // Получаем адрес электронной почты в зависимости от окружения
-          // Обновляем статус отправки для операции, если уведомление успешно отправлено
-          await pool.query(
-            `
-        UPDATE dbo.tool_history_nom
-        SET sent = TRUE
-        WHERE specs_op_id = $1
-      `,
-            [specsOpId]
-          )
-        }
-      })
     }
+
+    await transporter.sendMail(mailOptions)
+    console.log(`Отчет по инструментам для партии ${partId} отправлен на ${financeUserEmail}`)
+
+    // Обновляем статус отправки отчета в таблице tool_part_archive
+    await pool.query(
+      `
+        UPDATE dbo.tool_part_archive
+        SET report_sent_buch      = TRUE,
+            date_report_sent_buch = CURRENT_TIMESTAMP
+        WHERE specs_nom_id = $1
+      `,
+      [partId],
+    )
+
   } catch (error) {
-    console.error('Ошибка отправки уведомлений: ', error)
+    console.error(`Ошибка при отправке отчета для партии ${partId}:`, error)
   }
 }
 
-// Schedule the cron job
-// const min15 = '0 */15 * * * *'
-// const sec10 = '*/10 * * * * *'
-// cron.schedule(min15, () => {
-//   checkStatusChanges()
-//     .then(() => {
-//       // console.log('Задача выполнена успешно.')
-//     })
-//     .catch((error) => {
-//       console.error('Ошибка при выполнении задачи:', error)
-//     })
-// })
+async function checkStatusChanges() {
+  console.log("Checking status changes")
+  try {
+    const result = await pool.query(
+      `
+        SELECT sn.id AS part_id
+        FROM dbo.specs_nom sn
+        WHERE sn.prod_end_time <= NOW()
+          AND EXISTS (SELECT 1
+                      FROM dbo.tool_part_archive tpa
+                      WHERE tpa.specs_nom_id = sn.id
+                        AND tpa.report_sent_buch = FALSE)
+        ORDER BY sn.prod_end_time DESC
+        LIMIT 1
+      `,
+    )
+
+    if (result.rows.length > 0) {
+      console.log(part_id)
+      const partId = result.rows[0].part_id
+      await sendReportForPart(partId)
+    } else {
+      console.log('Нет партий для отправки отчетов.')
+    }
+  } catch (error) {
+    console.error('Ошибка при проверке статусов:', error)
+  }
+}
+
+// Запускаем проверку каждые 15 сек
+cron.schedule('*/15 * * * * *', checkStatusChanges)
 
 module.exports = { checkStatusChanges }
