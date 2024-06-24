e79924e453cc95b55905cd52bca4a37a450d7300
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-06-24 14:43:15 +0300

    Update OrderToolsController.js
    Update OrderToolsController.js
    
    Update OrderToolsController.js

--- backend/src/controllers/tool/reports/vue/OrderToolsController.js
+++ backend/src/controllers/tool/reports/vue/OrderToolsController.js
@@ -23,10 +23,10 @@ async function getTableReportData(req, res) {
                                   FROM dbo.tool_tree tt
                                          JOIN TreePath tp ON tt.parent_id = tp.id),
                      totals AS (SELECT group_id,
-                                        SUM(sklad) AS group_total_sklad
-                                 FROM dbo.tool_nom
-                                 GROUP BY group_id),
-                     damaged AS (SELECT tn.id                                     AS id_tool,
+                                       SUM(sklad) AS group_total_sklad
+                                FROM dbo.tool_nom
+                                GROUP BY group_id),
+                     damaged AS (SELECT tn.id                          AS id_tool,
                                         tn.parent_id,
                                         tn.group_id,
                                         tn.group_standard,
@@ -36,15 +36,19 @@ async function getTableReportData(req, res) {
                                         CASE
                                           WHEN t.group_total_sklad > 0 THEN GREATEST(tn.norma - t.group_total_sklad, 0)
                                           ELSE GREATEST(tn.norma - tn.sklad, 0)
-                                        END                                            AS zakaz,
-                                        COALESCE(SUM(thd.quantity), 0)           AS damaged_last_7_days,
-                                        t.group_total_sklad                      AS group_sum
+                                          END                          AS zakaz,
+                                        COALESCE(SUM(thd.quantity), 0) AS damaged_last_7_days,
+                                        t.group_total_sklad            AS group_sklad
                                  FROM dbo.tool_nom tn
-                                 LEFT JOIN dbo.tool_history_damaged thd ON tn.id = thd.id_tool
-                                 AND thd.timestamp >= CURRENT_DATE - INTERVAL '7 days'
-                                 LEFT JOIN totals t ON tn.group_id = t.group_id
-                                 WHERE tn.norma - tn.sklad > 0
-                                 GROUP BY tn.id, tn.parent_id, tn.name, tn.sklad, tn.norma, tn.group_id, tn.group_standard, t.group_total_sklad)
+                                        LEFT JOIN dbo.tool_history_damaged thd ON tn.id = thd.id_tool
+                                   AND thd.timestamp >= CURRENT_DATE - INTERVAL '7 days'
+                                        LEFT JOIN totals t ON tn.group_id = t.group_id
+                                 WHERE CASE
+                                         WHEN t.group_total_sklad > 0 THEN tn.norma - t.group_total_sklad > 0
+                                         ELSE tn.norma - tn.sklad > 0
+                                         END
+                                 GROUP BY tn.id, tn.parent_id, tn.name, tn.sklad, tn.norma, tn.group_id,
+                                          tn.group_standard, t.group_total_sklad)
                    SELECT d.parent_id,
                           tp.path,
                           JSON_AGG(
@@ -57,12 +61,13 @@ async function getTableReportData(req, res) {
                               'damaged_last_7_days', d.damaged_last_7_days,
                               'group_id', d.group_id,
                               'group_standard', d.group_standard,
-                              'group_sum', d.group_sum
+                              'group_sklad', d.group_sklad
                             )
                           ) AS tools
                    FROM damaged d
                           JOIN TreePath tp ON d.parent_id = tp.id
                    GROUP BY d.parent_id, tp.path
+                   HAVING SUM(d.zakaz) > 0 -- Фильтруем по сумме заказов
                    ORDER BY tp.path;
     `
     const { rows } = await pool.query(query)
