1076d93a338dc20fed90ca2d985f9a7475295eb8
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-06-27 16:05:24 +0300

    Update routers.js

--- backend/src/routers.js
+++ backend/src/routers.js
@@ -23,73 +23,68 @@ const reportVueBuhController = require('./controllers/tool/reports/vue/BuchWeekC
 
 const groupsController = require('./controllers/tool/main/GroupController')
 
-// const excelZakazController = require('./controllers/tool/reports/excel/b_order_Instr')
-
-// Маршруты для аутентификации
+// 'Аутентификация'
 router.post('/login', loginController.login)
 router.post('/check-login', loginController.checkLogin)
-
 router.get('/database-info', loginController.getDatabaseInfo)
-// nom
-router.get('/tool/:id', nomController.getToolById) //1 элемент
+
+// "Инструмент"
 router.get('/tools', nomController.getTools)
-// router.post('/tools', nomController.getTools) //POST ALL
 router.post('/tool', nomController.addTool)
 router.put('/tool/:id', nomController.editTool)
 router.delete('/tool/:id', nomController.deleteTool)
+// "Инструмент дополнительно"
+router.get('/tool/:id', nomController.getToolById) //1 элемент
 router.get('/filter-params/:parent_id', nomController.getFilterParamsByParentId)
 
-// param
+// "Параметры"
 router.get('/tools-params', paramController.getToolParams)
-
 router.post('/tools-params', paramController.addToolParam)
 router.put('/tools-params/:id', paramController.updateToolParam)
 router.delete('/tools-params/:id', paramController.deleteToolParam)
+// "Параметры дополнительно"
 router.patch('/tools-params/:id/move', paramController.moveToolParam)
 
-// tree
+// "Дерево"
 router.get('/tools-tree', treeController.getToolsTree)
 router.post('/tools-tree', treeController.addBranch)
 router.put('/tools-tree', treeController.updateFolderTree)
 router.delete('/tools-tree/:id', treeController.dellFolderTree)
 
-// issue
-router.get('/operators/fio', issueController.getFioOperators)
-
-router.get('/issue/cancel-operation-admin/:id', issueController.cancelOperationAdmin) // отмена любой
-router.post('/issue/cancel-operation/:id', issueController.cancelOperation) //отмена 3 дня
-
-router.post('/issue', issueController.issueTool)
+// "Выдача инструмента"
 router.post('/issues', issueController.issueTools)
-router.get('/cnc', issueController.getCncData)
+router.get('/cnc', issueController.getCncData) //форма заполнения
+router.get('/operators/fio', issueController.getFioOperators) //форма заполнения
+router.get('/issue/cancel-operation-admin/:id', issueController.cancelOperationAdmin) // отмена операции любой
+router.post('/issue/cancel-operation/:id', issueController.cancelOperation) //отмена операции 3 дня
 
-// history
-router.get('/history/:id', historyController.getToolHistoryId)
+// "История выдачи"
 router.get('/history', historyController.getToolHistory)
-router.get('/history-all-tool', historyController.getAllIssuedToolIdsWithNames)
+router.get('/history-all-tool', historyController.getAllIssuedToolIdsWithNames) //инструмент для поиска
+// router.get('/history/:id', historyController.getToolHistoryId)
+
+// "История выдачи Modal"
+router.get('/history-part', historyControllerModal.getToolHistoryByPartId) //история основной список
+router.get('/history-part/info', historyControllerModal.getToolHistoryByPartIdInfo) //история информация по партии
+router.post('/history-add-archive', historyControllerModal.addToArchive) //архив истории выдачи
+
+// "Движение инструмента"
 router.get('/tool-movement/:id', historyController.getToolMovementById)
 
-//groups
+// "Группы"
 router.get('/tools-groups', groupsController.getGroupedTools)
 
-router.get('/history-part', historyControllerModal.getToolHistoryByPartId)
-
-//Modal архив истории информация о
-router.get('/history-part/info', historyControllerModal.getToolHistoryByPartIdInfo)
-//Modal архив истории выдачи
-router.post('/history-add-archive', historyControllerModal.addToArchive)
-
-// damaged
+// "Поврежденный инструмент"
 router.get('/damaged-history', damagedController.getDamaged)
 router.post('/tool-history-damaged', damagedController.addToolHistoryDamaged)
 
-// email report
+// "Email report"
 router.get('/report/zayav-instr', reportZakazController.genZayavInstr) // заявка на инструмент	раз в неделю каждый ЧТ в 12:00 (за неделю)
 router.get('/report/revision-instr', reportRevisionController.genRevisionInstr) // заявка на инструмент	раз в неделю каждый ЧТ в 12:00 (за неделю)
-// email report Buch
+// "Email report Buch *CRON"
 router.get('/report/buch-end-op', reportBuchEndPartController.checkStatusChanges) //в режиме CRON
 
-//vue
+// "Vue"
 router.get('/report/get-zakaz', reportVueZakazController.getTableReportData) // заявка на инструмент	раз в неделю каждый ЧТ в 12:00 (за неделю)
 router.get('/report/getBuchWeek', reportVueBuhController.getTableReportData) //❓ бухгалтерию исключен сломанный	раз в неделю каждый ПТ в 12:00 (за неделю)
 
