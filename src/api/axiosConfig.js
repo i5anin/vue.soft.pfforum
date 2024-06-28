88924a5988f8c888955cad2354f054179c32acf5
Isanin Sergey <92434473+i5anin@users.noreply.github.com>
2024-06-28 10:30:00 +0300

    Update axiosConfig.js

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -6,9 +6,7 @@ const getBaseUrl = (serverType) => {
     case 'node':
       return import.meta.env.VITE_BASE_URL || 'http://localhost:4000/api'
     case 'laravel':
-      return (
-        import.meta.env.VITE_BASE_URL_LARAVEL || 'http://localhost:8000/api'
-      )
+      return import.meta.env.VITE_BASE_URL_LARAVEL || 'http://localhost:8000/api'
     default:
       return 'http://localhost:4000/api' // Введите значение по умолчанию или выбросите ошибку
   }
